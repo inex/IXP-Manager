7455cd91237eb4f2fd6ad6da6f42481e0f39615e
Barry O'Donovan <barry@opensolutions.ie>
2011-06-07 20:54:42 +0100

    Matching type to user.id.

--- application/models/generated/BaseChangeLog.php
+++ application/models/generated/BaseChangeLog.php
@@ -69,7 +69,7 @@ abstract class BaseChangeLog extends Doctrine_Record
              'type' => 'integer',
              'length' => 4,
              'fixed' => false,
-             'unsigned' => false,
+             'unsigned' => true,
              'primary' => false,
              'notnull' => true,
              'autoincrement' => false,
