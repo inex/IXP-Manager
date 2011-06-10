cedaf35b8b7d734290dee8597747abfbeec5bd4e
Barry O'Donovan <barry@opensolutions.ie>
2011-06-10 11:15:14 +0100

    Two utility scripts for populating the IPv4 and v6 address tables.

---
+++ bin/gen-sql-ipv4-inserts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $# -ne 4 ]]; then
+    echo "IXP Manager - A web application to assist in the management of IXPs"
+    echo "Copyright (C) 2009-2011 Internet Neutral Exchange Association Limited."
+    echo
+    echo "gen-sql-ipv4-inserts.sh - a utility script to generate SQL INSERT statements"
+    echo "    to populate the IPv4 table."
+    echo
+    echo "Usage: $0 [vlanid] [start of address] [start octet] [end octet]"
+    echo
+    echo "E.g. $0 1 192.168.0. 10 20"
+    echo
+fi;
+
+for i in `seq $3 $4`; do
+    echo "INSERT INTO \`ipv4address\` ( \`address\`, \`vlanid\` ) VALUES ( '$2$i', '$1' );"
+done
+
+ 
