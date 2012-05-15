c355ed7509ba0c4cfebbe1301c13642c5baa8fe0
Barry O'Donovan <barry@opensolutions.ie>
2012-05-15 20:59:00 +0100

    Version bump

--- library/INEX/Version.php
+++ library/INEX/Version.php
@@ -3,5 +3,5 @@
 # Basic version information
 #
 
-define( 'APPLICATION_VERSION', '2.1.5' );
-define( 'APPLICATION_VERDATE', '20120515203900' );
+define( 'APPLICATION_VERSION', '2.1.6' );
+define( 'APPLICATION_VERDATE', '20120515205800' );
