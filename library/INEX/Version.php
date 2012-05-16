7d0033a4958241b294a691f8663d215f15b2f8fa
Barry O'Donovan <barry@opensolutions.ie>
2012-05-16 15:01:30 +0100

    Freshly pressed CSS/JS and version bump

--- library/INEX/Version.php
+++ library/INEX/Version.php
@@ -3,5 +3,5 @@
 # Basic version information
 #
 
-define( 'APPLICATION_VERSION', '2.1.6' );
-define( 'APPLICATION_VERDATE', '20120515205800' );
+define( 'APPLICATION_VERSION', '2.2.0' );
+define( 'APPLICATION_VERDATE', '20120516145900' );
