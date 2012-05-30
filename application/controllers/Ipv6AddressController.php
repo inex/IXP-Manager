91565c3419069ac2010c4614508febca232412ff
Barry O'Donovan <barry@opensolutions.ie>
2012-05-30 08:10:10 +0100

    Better ordering for IP address lists.

--- application/controllers/Ipv6AddressController.php
+++ application/controllers/Ipv6AddressController.php
@@ -3,21 +3,21 @@
 /*
  * Copyright (C) 2009-2011 Internet Neutral Exchange Association Limited.
  * All Rights Reserved.
- * 
+ *
  * This file is part of IXP Manager.
- * 
+ *
  * IXP Manager is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, version v2.0 of the License.
- * 
+ *
  * IXP Manager is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License v2.0
  * along with IXP Manager.  If not, see:
- * 
+ *
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
@@ -79,7 +79,7 @@ class Ipv6AddressController extends INEX_Controller_FrontEnd
     {
         $this->view->vlans = Doctrine_Query::create()
             ->from( 'Vlan v' )
-            ->orderBy( 'v.name ASC' )
+            ->orderBy( 'v.number ASC' )
             ->fetchArray();
         
         if( count( $this->view->vlans ) == 0 )
