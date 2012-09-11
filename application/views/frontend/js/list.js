dfe23b2b00a3ecc5dd2538c355aa8ee8356cf3bc
Barry O'Donovan <barry@opensolutions.ie>
2012-09-11 14:25:33 +0100

    Add addon JS option, stop table from flicking

--- application/views/frontend/js/list.js
+++ application/views/frontend/js/list.js
@@ -3,12 +3,12 @@
 var oDataTable;
 
 $(document).ready(function() {
-	
+
 	$( 'a[id|="list-delete"]' ).on( 'click', function( event ){
-		
+
 		event.preventDefault();
 		url = $(this).attr("href");
-		
+
 	    bootbox.dialog( "Are you sure you want to delete this object?", [{
 	    	"label": "Cancel",
 	    	"class": "btn-primary"
@@ -18,10 +18,10 @@ $(document).ready(function() {
 	    	"class": "btn-danger",
 	    	"callback": function() { document.location.href = url; }
 	    }]);
-    		
+
     });
-	
-	
+
+
 	oDataTable = $( '#frontend-list-table' ).dataTable({
         'fnDrawCallback': function() {
                 if( oss_prefs != undefined && 'iLength' in oss_prefs && oss_prefs['iLength'] != $( "select[name='frontend-list-table_length']" ).val() )
@@ -30,8 +30,8 @@ $(document).ready(function() {
                     $.jsonCookie( 'oss_prefs', oss_prefs, oss_cookie_options );
                 }
             },
-        'iDisplayLength': ( typeof oss_prefs != 'undefined' && 'iLength' in oss_prefs ) 
-        		? oss_prefs['iLength'] 
+        'iDisplayLength': ( typeof oss_prefs != 'undefined' && 'iLength' in oss_prefs )
+        		? oss_prefs['iLength']
             	: {if isset( $options.defaults.table.entries )}{$options.defaults.table.entries}{else}10{/if},
         "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
         "sPaginationType": "bootstrap",
@@ -44,7 +44,8 @@ $(document).ready(function() {
             { 'bSortable': false, "bSearchable": false }
         ]
     });
-    
+    $( '#frontend-list-table' ).show();
+
 });
 
 
