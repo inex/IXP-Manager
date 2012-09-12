b320686243cb9a1f9bfd6e1f4979ea00bc6df456
Barry O'Donovan <barry@opensolutions.ie>
2012-09-12 15:02:18 +0100

    Utility function for creating dropdown list

--- library/INEX/Form/Cabinet.php
+++ library/INEX/Form/Cabinet.php
@@ -3,21 +3,21 @@
 /*
  * Copyright (C) 2009-2011 Internet Neutral Exchange Association Limited.
  * All Rights Reserved.
- * 
+ *
  * This file is part of IXP Manager.
- * 
+ *
  * IXP Manager is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation, version v2.0 of the License.
- * 
+ *
  * IXP Manager is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License v2.0
  * along with IXP Manager.  If not, see:
- * 
+ *
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
@@ -129,6 +129,27 @@ class INEX_Form_Cabinet extends INEX_Form
 
     }
 
+    
+    /**
+     * Create a SELECT / dropdown element of all cabinet names indexed by their id.
+     *
+     * @param string $name The element name
+     * @return Zend_Form_Element_Select The select element
+     */
+    public static function getPopulatedSelect( $name = 'cabinetid' )
+    {
+        $cab = new Zend_Form_Element_Select( $name );
+        
+        $maxId = self::populateSelectFromDatabase( $cab, '\\Entities\\Cabinet', 'id', 'name', 'name', 'ASC' );
+        
+        $cab->setRegisterInArrayValidator( true )
+            ->setRequired( true )
+            ->setLabel( _( 'Cabinet' ) )
+            ->setAttrib( 'class', 'span3 chzn-select' )
+            ->addValidator( 'between', false, array( 1, $maxId ) )
+            ->setErrorMessages( array( _( 'Please select a cabinet' ) ) );
+        
+        return $cab;
+    }
 }
 
-?>
