b688a0946ea6b3a4daa18c513065822f52802010
Barry O'Donovan <barry@opensolutions.ie>
2012-09-18 15:43:55 +0100

    Switch to and from user is now working

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -82,152 +82,59 @@ class AuthController extends INEX_Controller_Action
             $this->_forward( 'index', 'dashboard' );
     }
 
+    
+    
     /**
-     * Switch the logged in user to another.
+     * This function is called just before `switchUserAction()` processes anything.
      *
-     * Allows administrators to switch to another user and operate as them temporarily.
+     * @return bool True unless you want the switch to fail.
      */
-    public function switchAction()
+    protected function _switchUserPreCheck()
     {
-        // only super admins can switch user!
-        if( $this->user['privs'] != User::AUTH_SUPERUSER )
+        if( $this->getUser()->getPrivs() != \Entities\User::AUTH_SUPERUSER )
         {
-            $this->getLogger()->notice( 'User ' . $this->user['username'] . ' tried to switch to user with ID '
-                . $this->_request->getParam( 'id', '[unknown]' ) );
-            $this->session->message = new INEX_Message(
+            $this->getLogger()->notice( 'User ' . $this->getUser()->getUsername() . ' illegally tried to switch to user with ID '
+                . $this->_getParam( 'id', '[unknown]' )
+            );
+            
+            $this->addMessage(
                 'You are not allowed to switch users! This attempt has been logged and the administrators notified.',
-                INEX_Message::MESSAGE_TYPE_ERROR
+                OSS_Message::ERROR
             );
-
-            if( $this->user['privs'] == User::AUTH_CUSTADMIN )
-            {
-                $this->_redirect( 'cust-admin/users' );
-            }
-            else
-            {
-                $this->_redirect( 'dashboard' );
-            }
+            
+            $this->redirectAndEnsureDie( '' );
         }
-
-        // store the fact that we're switching in the session
-        $this->session->switched_user_from = $this->user['id'];
-
-        // does the requested user exist
-        $nu = Doctrine_Core::getTable( 'User' )->find( $this->_request->getParam( 'id', false ) );
-
-        if( !$nu )
-        {
-            $this->session->message = new INEX_Message(
-                'The requested user does not exist',
-                INEX_Message::MESSAGE_TYPE_ERROR
-            );
-
-            $this->_redirect( 'user' );
-        }
-
-        // easiest way to switch users is to just re-autenticate as the new one
-        // This maintains consistancy with Zend_Auth and future changes
-        $result = $this->_reauthenticate( $nu );
-
-        if( $result->getCode() == Zend_Auth_Result::SUCCESS )
-        {
-            $this->getLogger()->notice( 'User ' . $this->user['username'] . ' has switched to user '
-                . $nu['username'] );
-
-            $this->session->message = new INEX_Message(
-                "You are now logged in as {$nu['username']}.", INEX_Message::MESSAGE_TYPE_SUCCESS
-            );
-        }
-        else
-        {
-            $this->getLogger()->notice( 'User ' . $this->user['username'] . ' has failed to switch to user '
-                . $nu['username'] );
-
-            $this->session->message = new INEX_Message(
-                "Error: Could not switch user.", INEX_Message::MESSAGE_TYPE_ERROR
-            );
-
-            $this->_redirect( 'user' );
-        }
-
-        if( $nu['privs'] == User::AUTH_CUSTADMIN )
-            $this->_redirect( 'cust-admin/users' );
-        else
-            $this->_redirect( 'dashboard' );
+        
+        return true;
     }
 
     /**
-     * Switch back to the original user when switched to another.
+     * This function is called after `switchUserAction()` loads the requested user object.
      *
-     * Allows administrators to switch back from another user who they operated as them temporarily.
+     * @param \Entities\User $nuser The user to switch to
+     * @return bool True unless you want the switch to fail.
      */
-    public function switchBackAction()
+    protected function _switchUserCheck( $nuser )
     {
-        // are we really operating as another?
-        if( !isset( $this->session->switched_user_from ) or !$this->session->switched_user_from )
-        {
-            $this->session->message = new INEX_Message(
-                'You are not currently logged in as another user. You are: ' . $this->user['username'],
-                INEX_Message::MESSAGE_TYPE_ERROR
-            );
-
-            if( $this->user['privs'] == User::AUTH_SUPERUSER )
-            {
-                $this->_redirect( 'user' );
-            }
-            else if( $this->user['privs'] == User::AUTH_CUSTADMIN )
-            {
-                $this->_redirect( 'cust-admin/users' );
-            }
-            else
-            {
-                $this->_redirect( 'dashboard' );
-            }
-        }
-
+        return true;
+    }
+    
+    /**
+     * This function is called just before `switchUserBackAction()` actually switches
+     * the user back.
+     *
+     * @param \Entities\User $subUser The current user we have switched to (substituted to)
+     * @param \Entities\User $origUser The original user that we switched from
+     * @return bool|array False if you want the switch back to fail.
+     */
+    protected function _switchUserBackCheck( $subUser, $origUser )
+    {
         // record current user customer ID
         $custid = $this->getUser()->custid;
         
-        // does the original user exist
-        $ou = Doctrine_Core::getTable( 'User' )->find( $this->session->switched_user_from );
-
-        if( !$ou )
-            die( 'The user you are trying to switch back to no longer exists!!' );
-
-        // easiest way to switch users is to just re-autenticate as the new one
-        // This maintains consistancy with Zend_Auth and future changes
-        $result = $this->_reauthenticate( $ou );
-
-        if( $result->getCode() == Zend_Auth_Result::SUCCESS )
-        {
-            $this->getLogger()->info( 'User ' . $ou['username'] . ' has switched back from user '
-                . $this->user['username'] );
-
-            $this->session->message = new INEX_Message(
-                "You are now logged in as {$ou['username']}.", INEX_Message::MESSAGE_TYPE_SUCCESS
-            );
-        }
-        else
-            die( 'Could not switch back!!' );
-
-        unset( $this->session->switched_user_from );
-        $this->_redirect( 'customer/dashboard/id/' . $custid );
+        $params['url'] = 'customer/dashboard/id/' . $subUser->getCustomer()->getId();
+        
+        return $params;
     }
-
-    /**
-     * A simple private function to reauthenticate to a given user.
-     *
-     * @param User $user A Doctrine user object
-     */
-    private function _reauthenticate( $user )
-    {
-        $auth = Zend_Auth::getInstance();
-
-        $authAdapter = new INEX_Auth_DoctrineAdapter(
-            $user['username'], $user['password']
-        );
-
-        return $auth->authenticate( $authAdapter );
-    }
-
+    
 }
