e343afd46fd19fb4e259de7afb7dc973c23666e9
Charlie Root <root@sflow.inex.ie>
2013-03-09 17:03:30 +0000

    [IM] de-bashification.  not all operating systems are linux.

--- bin/library-init.sh
+++ bin/library-init.sh
@@ -1,20 +1,22 @@
-#! /usr/bin/env bash
+#!/bin/sh
 
 # This file will set up Git / SVN externals in library/
 
 
 # Is SVN installed and in the path?
 
-svn &>/dev/null
 
-if [[ $? -eq 127 ]]; then
+
+svn > /dev/null 2>&1
+
+if [ $? -eq 127 ]; then
     echo ERROR: SVN not installed or not in the path
     exit
 fi
 
-git &>/dev/null
+git > /dev/null 2>&1
 
-if [[ $? -eq 127 ]]; then
+if [ $? -eq 127 ]; then
     echo ERROR: Git not installed or not in the path
     exit
 fi
@@ -23,8 +25,7 @@ LIBDIR=`dirname "$0"`/../library
 TOPDIR=`dirname "$0"`/..
 
 # Smarty
-
-if [[ -e $LIBDIR/Smarty ]]; then
+if [ -e $LIBDIR/Smarty ]; then
     echo Smarty exists - skipping!
 else
     svn co http://smarty-php.googlecode.com/svn/trunk/distribution/libs/ $LIBDIR/Smarty
@@ -32,28 +33,28 @@ fi
 
 
 # Twitter form decorators
-if [[ -e $LIBDIR/Bootstrap-Zend-Framework ]]; then
+if [ -e $LIBDIR/Bootstrap-Zend-Framework ]; then
     echo Bootstrap-Zend-Framework exists - skipping!
 else
     git clone git://github.com/opensolutions/Bootstrap-Zend-Framework.git $LIBDIR/Bootstrap-Zend-Framework
 fi
 
 # Minifier
-if [[ -e $LIBDIR/Minify ]]; then
+if [ -e $LIBDIR/Minify ]; then
     echo Minify exists - skipping!
 else
     git clone git://github.com/opensolutions/Minify.git $LIBDIR/Minify
 fi
 
 # Bootbox
-if [[ -e $LIBDIR/Bootbox ]]; then
+if [ -e $LIBDIR/Bootbox ]; then
     echo Bootbox exists - skipping!
 else
     git clone https://github.com/makeusabrew/bootbox.git $LIBDIR/Bootbox
 fi
 
 # Throbber.js
-if [[ -e $LIBDIR/Throbber.js ]]; then
+if [ -e $LIBDIR/Throbber.js ]; then
     echo Throbber.js exists - skipping!
 else
     git clone https://github.com/aino/throbber.js.git $LIBDIR/Throbber.js
@@ -62,7 +63,7 @@ fi
 
 # Zend
 
-if [[ -e $LIBDIR/Zend ]]; then
+if [ -e $LIBDIR/Zend ]; then
     echo Zend exists - skipping!
 else 
     svn co http://framework.zend.com/svn/framework/standard/branches/release-1.12/library/Zend/ $LIBDIR/Zend
@@ -70,7 +71,7 @@ fi
         
 
 # OSS-Framework
-if [[ -e $LIBDIR/OSS-Framework.git ]]; then
+if [ -e $LIBDIR/OSS-Framework.git ]; then
     echo OSS-Framework.git exists - skipping!
 else
     git clone git://github.com/opensolutions/OSS-Framework.git $LIBDIR/OSS-Framework.git
