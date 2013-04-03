9387a988029e30da011c82689ae68e0d407079b8
Barry O'Donovan <barry@opensolutions.ie>
2013-04-03 14:30:28 +0100

    Customer note updated field should be NOT NULL

--- library/IXP/Version.php
+++ library/IXP/Version.php
@@ -3,11 +3,6 @@
 # Basic version information
 #
 
-<<<<<<< HEAD
-define( 'APPLICATION_VERSION', '3.0.7' );
-define( 'APPLICATION_VERDATE', '20130403140000' );
-=======
 define( 'APPLICATION_VERSION', '3.0.8' );
 define( 'APPLICATION_VERDATE', '20130403140400' );
->>>>>>> nf-member-notes
 
