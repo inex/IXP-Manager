44dc2da5e4c0cb663e1756f702d4999e79e50697
Nerijus Barauskas <nerijus@opensolutions.ie>
2013-04-09 11:41:32 +0100

    [IM] New message then removing contact or user

---
+++ application/views/contact/js/list-addon.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	$( 'a[id|="list-delete"]' ).off('click').on( 'click', function( event ){
+
+		event.preventDefault();
+		url = $(this).attr("href");
+
+	    bootbox.dialog( "Are you sure you want to delete this contact? Related user also will be deleted.", [{
+	    	"label": "Cancel",
+	    	"class": "btn-primary"
+	    },
+	    {
+	    	"label": "Delete",
+	    	"class": "btn-danger",
+	    	"callback": function() { document.location.href = url; }
+	    }]);
+
+    });
+});
+
+
+
+
