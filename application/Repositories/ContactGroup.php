1eb6ac77cce4b6a2573b35dc49d0b75e0c92d54d
Barry O'Donovan <barry@opensolutions.ie>
2013-04-09 11:44:27 +0100

    [IM] Fn to count contact groups of a given type for a given customer

--- application/Repositories/ContactGroup.php
+++ application/Repositories/ContactGroup.php
@@ -13,21 +13,22 @@ use Doctrine\ORM\EntityRepository;
 class ContactGroup extends EntityRepository
 {
     /**
-     * Gets groups name array.
+     * Get contact group names as an array grouped by group type.
      *
-     * Returned array structure: 
-     * $arr = [
-     *      'ROLE' => [
-     *           [ 'id' => 1, 'name' => 'Billing' ],
-     *           [ 'id' => 2, 'name' => 'Admin']...
-     *       ]
-     *       'Other' => [
-     *           [ 'id' => n, 'name' => 'Other group' ]
-     *       ]
-     * ];
-     * 
-     * @param string|bool $type Contact group tipe
-     * @param int|bool    $cid  Concact id to filter for particular contact
+     * Returned array structure:
+     *
+     *     $arr = [
+     *         'ROLE' => [
+     *              [ 'id' => 1, 'name' => 'Billing' ],
+     *              [ 'id' => 2, 'name' => 'Admin']
+     *         ]
+     *         'OTHER' => [
+     *              [ 'id' => n, 'name' => 'Other group' ]
+     *         ]
+     *     ];
+     *
+     * @param string $type Optionally limit to a specific type
+     * @param int    $cid  Contact id to filter for a particular contact
      * @return array
      */
     public function getGroupNamesTypeArray( $type = false, $cid = false )
@@ -40,21 +41,19 @@ class ContactGroup extends EntityRepository
             
         $dql .= " WHERE cg.active = 1";
             
-        if( $type && $cid )
-            $dql .= " AND cg.type = ?1 AND c.id = ?2";
-        else if( $type )
+        if( $type )
             $dql .= " AND cg.type = ?1";
-        else if( $cid )
-            $dql .= " AND c.id = ?1";
+        
+        if( $cid )
+            $dql .= " AND c.id = ?2";
         
         $q = $this->getEntityManager()->createQuery( $dql );
             
-        if( $type && $cid )
-            $q->setParameter( 1, $type)->setParameter( 2, $cid );
-        else if( $type  )
+        if( $type  )
            $q->setParameter( 1, $type );
-        else if( !$type && $cid )
-            $q->setParameter( 1, $cid );
+        
+        if( $cid )
+            $q->setParameter( 2, $cid );
             
         $tmpGroups = $q->getArrayResult();
 
@@ -63,5 +62,28 @@ class ContactGroup extends EntityRepository
             $groups[ $g['type'] ][ $g[ 'id' ] ] = [ 'id' => $g['id'], 'name' => $g['name'] ];
 
         return $groups;
+    }
+
+    /**
+     * Get the number of contacts with a contact group for a given customer.
+     *
+     * @param \Entities\Customer $customer The customer to count the contact groups of
+     * @param int $id Contact group id
+     * @return int The number of contacts with a contact group for a given customer
+     */
+    public function countForCustomer( $customer, $id )
+    {
+        return $this->getEntityManager()->createQuery(
+            "SELECT COUNT( cg.id )
+                FROM \\Entities\\ContactGroup cg
+                    LEFT JOIN cg.Contacts c
+                    LEFT JOIN c.Customer cust
+                WHERE
+                    cg.id = ?1
+                    AND cust = ?2"
+            )
+            ->setParameter( 1, $id )
+            ->setParameter( 2, $customer )
+            ->getSingleScalarResult();
     }
 }
