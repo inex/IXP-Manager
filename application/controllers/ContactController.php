da8db5a594f1f5c43319f9169f01709a13b6d9fe
Barry O'Donovan <barry@opensolutions.ie>
2013-04-10 09:27:53 +0100

    [IM] Destination on add / edit / cancel working for customer overview tabs

--- application/controllers/ContactController.php
+++ application/controllers/ContactController.php
@@ -186,6 +186,17 @@ class ContactController extends IXP_Controller_FrontEnd
         else if( $this->getParam( "uid", false ) )
             $form->setAction( OSS_Utils::genUrl( 'contact', ( $isEdit ? 'edit' : 'add' ), false, [ 'uid' => $this->getParam( "uid" ) ] ) );
 
+        if( $cid = $this->getParam( 'cid', false ) )
+        {
+            $form->updateCancelLocation( OSS_Utils::genUrl( 'customer', 'overview', false,
+                    [
+                        'id' => $cid,
+                        'tab' => ( $this->getParam( 'user', false ) || $this->getParam( 'uid', false ) ) ? 'users' : 'contacts'
+                    ]
+                )
+            );
+        }
+        
         if( $isEdit )
         {
             $form->getElement( 'custid' )->setValue( $object->getCustomer()->getId() );
