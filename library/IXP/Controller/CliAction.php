9ee6bafe400bcbe219565611e2848650e85f5356
Barry O'Donovan <barry@opensolutions.ie>
2013-04-24 09:06:44 +0100

    [NF] New CLI controller for switch functions

---
+++ library/IXP/Controller/CliAction.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * Copyright (C) 2009-2012 Internet Neutral Exchange Association Limited.
+ * All Rights Reserved.
+ *
+ * This file is part of IXP Manager.
+ *
+ * IXP Manager is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, version v2.0 of the License.
+ *
+ * IXP Manager is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License v2.0
+ * along with IXP Manager.  If not, see:
+ *
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+
+/**
+ * INEX's version of Zend's Zend_Controller_Action implemented custom
+ * functionality.
+ *
+ * All application controlers subclass this rather than Zend's version directly.
+ *
+ * @package IXP_Controller
+ *
+ */
+class IXP_Controller_CliAction extends OSS_Controller_Action
+{
+
+    // traits we want to use
+    use OSS_Controller_Action_Trait_Doctrine2Cache;
+    use OSS_Controller_Action_Trait_Doctrine2;
+    use OSS_Controller_Action_Trait_Mailer;
+    use OSS_Controller_Action_Trait_Logger;
+    
+    /**
+     * Verbose flag
+     */
+    private $_verbose = false;
+    
+    /**
+     * Override the Zend_Controller_Action's constructor (which is called
+     * at the very beginning of this function anyway).
+     *
+     * @param object $request See Parent class constructer
+     * @param object $response See Parent class constructer
+     * @param object $invokeArgs See Parent class constructer
+     */
+    public function __construct(
+                Zend_Controller_Request_Abstract $request,
+                Zend_Controller_Response_Abstract $response,
+                array $invokeArgs = null )
+    {
+        // call the parent's version where all the Zend magic happens
+        parent::__construct( $request, $response, $invokeArgs );
+
+        //Zend_Controller_Action_HelperBroker::removeHelper('viewRenderer');
+        if ( php_sapi_name() != 'cli' )
+        {
+            $this->getLogger()->warn( 'Non CLI access to a CLI controller from ' . $_SERVER['REMOTE_ADDR'] . ' tp ' . $_SERVER['REQUEST_URI'] );
+            die( 'Unauthorised access to action!' );
+        }
+        
+        $this->_verbose = $this->getFrontController()->getParam( 'verbose' );
+    }
+
+}
+
