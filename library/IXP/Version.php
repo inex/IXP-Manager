4027c21be260349a5e2952dc6748ea0813dcec87
Barry O'Donovan <barry@opensolutions.ie>
2013-04-30 11:48:34 +0100

    [NF] Add ifIndex to SwitchPort when polling via SNMP

--- library/IXP/Version.php
+++ library/IXP/Version.php
@@ -3,6 +3,6 @@
 # Basic version information
 #
 
-define( 'APPLICATION_VERSION', '3.0.10' );
-define( 'APPLICATION_VERDATE', '20130424204100' );
+define( 'APPLICATION_VERSION', '3.0.11' );
+define( 'APPLICATION_VERDATE', '20130430114500' );
 
