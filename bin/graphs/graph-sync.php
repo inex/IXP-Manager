510c4af32a82fbe83926fb2e70dc167b130173b4
Barry O'Donovan <barry@opensolutions.ie>
2013-06-19 10:18:53 +0100

    [IM] Update / improve / bugfix migration script

--- bin/graphs/graph-sync.php
+++ bin/graphs/graph-sync.php
@@ -1,6 +1,27 @@
 #!/usr/bin/env php
 <?php
 
+/*
+ * Copyright (C) 2009-2013 Internet Neutral Exchange Association Limited.
+ * All Rights Reserved.
+ *
+ * This file is part of IXP Manager.
+ *
+ * IXP Manager is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, version v2.0 of the License.
+ *
+ * IXP Manager is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License v2.0
+ * along with IXP Manager.  If not, see:
+ *
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+                   
 if( $argc < 5 )
 {
     echo "ERROR: Missing Parameters.\nUsage: graph-sync.php --mrtg <filename> --rrd <filename> [--out-mrtg <file>]\n";
@@ -43,61 +64,65 @@ if( !$mrtg || !$rrd )
     exit( 1 );
 }
 
-$handle = @fopen( $mrtg, "r" );
+if( !( $handle = @fopen( $mrtg, "r" ) ) )
+    die( "ERROR: cannot open $mrtg\n" );
 
 //script overwrites $out file use a to append $out file.
 $dir = dirname( $out );
 if( !is_dir( $dir ) )
-    mkdir( $dir, 777, true );
+    mkdir( $dir, 700, true );
 
-$fout = @fopen( $out, "w" );
+if( !( $fout = @fopen( $out, "w" ) ) )
+    die( "ERROR: cannot open $out\n" );
 
-if( $handle && fgets( $handle, 4096 ) !== false )  // skip first line
+fputs( $fout, fgets( $handle, 4096 ) ); // copy (and skip) first line
+
+$end = false;
+$previous = false;
+
+while( ( $buffer = fgets( $handle, 4096 ) ) !== false )
 {
-    $end = false;
-    $previous = false;
+    $row = explode( ' ', $buffer );
 
-    while( ( $buffer = fgets( $handle, 4096 ) ) !== false )
+    if( !$end )
     {
-        $row = explode( ' ', $buffer );
-
-        if( !$end )
-        {
-            $end = $row[0];
-            $previous = $buffer;
-            continue;
-        }
-
-        if( $end < rrd_first( $rrd ) && $row[1] == "0" && $row[2] == "0" )
-        {
-            $avg = getData( $rrd, 'AVERAGE', $row[0], $end );
-            $max = getData( $rrd, 'MAX', $row[0], $end );
-            
-            fputs( $fout, "{$row[0]} {$avg['in']} {$avg['out']} {$max['in']} {$max['out']}\n" );
-
-            /*echo "MRTG: $buffer";
-            echo "RRD:  {$end} {$avg['in']} {$avg['out']} {$max['in']} {$max['out']}\n";
-
-            echo sprintf( "DIFF: {$end} %0.3f %0.3f %0.3f %0.3f\n",
-                abs( ( ( $avg['in']  - $row[1] ) * 100 ) / $row[1] ),
-                abs( ( ( $avg['out'] - $row[2] ) * 100 ) / $row[2] ),
-                abs( ( ( $max['in']  - $row[3] ) * 100 ) / $row[3] ),
-                abs( ( ( $max['out'] - $row[4] ) * 100 ) / $row[4] )
-            );
-
-            echo "\n";*/
-        }
-
         $end = $row[0];
         $previous = $buffer;
+        fputs( $fout, $buffer );
+        continue;
     }
 
-    if( !feof( $handle ) )
-        echo "Error: unexpected fgets() fail\n";
+    if( $end < rrd_last( $rrd ) && $row[1] == "0" && $row[2] == "0" )
+    {
+        $avg = getData( $rrd, 'AVERAGE', $row[0], $end );
+        $max = getData( $rrd, 'MAX', $row[0], $end );
+            
+        fputs( $fout, "{$row[0]} {$avg['in']} {$avg['out']} {$max['in']} {$max['out']}\n" );
 
-    fclose( $handle );
-    fclose( $fout );
+        /*echo "MRTG: $buffer";
+        echo "RRD:  {$end} {$avg['in']} {$avg['out']} {$max['in']} {$max['out']}\n";
+ 
+        echo sprintf( "DIFF: {$end} %0.3f %0.3f %0.3f %0.3f\n",
+            abs( ( ( $avg['in']  - $row[1] ) * 100 ) / $row[1] ),
+            abs( ( ( $avg['out'] - $row[2] ) * 100 ) / $row[2] ),
+            abs( ( ( $max['in']  - $row[3] ) * 100 ) / $row[3] ),
+            abs( ( ( $max['out'] - $row[4] ) * 100 ) / $row[4] )
+        );
+
+        echo "\n";*/
+    }
+    else
+        fputs( $fout, $buffer );
+
+    $end = $row[0];
+    $previous = $buffer;
 }
+
+if( !feof( $handle ) )
+    echo "Error: unexpected fgets() fail\n";
+
+fclose( $handle );
+fclose( $fout );
 
 
 function getData( $rrd, $type, $start, $end  )
--- bin/graphs/migrate-graphs.php
+++ bin/graphs/migrate-graphs.php
@@ -1,6 +1,31 @@
 #!/usr/bin/env php
 <?php
 
+/*
+ * Copyright (C) 2009-2013 Internet Neutral Exchange Association Limited.
+ * All Rights Reserved.
+ *
+ * This file is part of IXP Manager.
+ *
+ * IXP Manager is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, version v2.0 of the License.
+ *
+ * IXP Manager is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License v2.0
+ * along with IXP Manager.  If not, see:
+ *
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+$SRCDIR_LOG  = "/srv/mrtg-test";
+$SRCDIR_RRD  = "/srv/cricket";
+$DESTDIR_LOG = "/srv/mrtg";
+                   
 mb_internal_encoding('UTF-8');
 mb_language('uni');
 setlocale(LC_ALL, "en_IE.utf8");
@@ -30,10 +55,15 @@ $application->getBootstrap()->registerPluginResource( $plugin );
 $em = $plugin->getDoctrine2( 'default' );
 
 
-$SRCDIR_LOG = "/home/nerijus/tmp/ixp-lonap";
-$SRCDIR_RRD = "/home/nerijus/tmp/ixp-lonap";
-$DESTDIR_LOG = "/home/nerijus/tmp/test-lonap";
+$count = 0;
+foreach( $em->getRepository( '\\Entities\\Customer' )->findAll() as $cust )
+    foreach( $cust->getVirtualInterfaces() as $viInt )
+        foreach( $viInt->getPhysicalInterfaces() as $pInt )
+            $count++;
 
+echo "#! /bin/sh\n\n";
+
+$i = 0;
 foreach( $em->getRepository( '\\Entities\\Customer' )->findAll() as $cust )
 {   
     foreach( $cust->getVirtualInterfaces() as $viInt )
@@ -44,19 +74,22 @@ foreach( $em->getRepository( '\\Entities\\Customer' )->findAll() as $cust )
             $port = $pInt->getSwitchPort()->getName();
             $port = str_replace( " ", "_", strtolower( $port ) );
             
-            $mrtg = sprintf( "%s/mrtg/members/%s/%s-%d-bits.log", $SRCDIR_LOG, $cust->getShortname(), $cust->getShortname(), $pInt->getMonitorindex() );
-            $rrd = sprintf( "%s/cricket/%s/%s.rrd", $SRCDIR_RRD, $switch, $port );
-            $out = sprintf( "%s/mrtg/members/%s/%s-%d-bits.log", $DESTDIR_LOG, $cust->getShortname(), $cust->getShortname(), $pInt->getMonitorindex() );
+            $mrtg = sprintf( "%s/members/%s/%s-%d-bits.log", $SRCDIR_LOG, $cust->getShortname(), $cust->getShortname(), $pInt->getMonitorindex() );
+            $rrd = sprintf( "%s/%s/%s.rrd", $SRCDIR_RRD, $switch, $port );
+            $out = sprintf( "%s/members/%s/%s-%d-bits.log", $DESTDIR_LOG, $cust->getShortname(), $cust->getShortname(), $pInt->getMonitorindex() );
             
             //echo "MRTG: $mrtg \n RRD: $rrd\n";
             if( !file_exists( $mrtg ) || !file_exists( $rrd ) )
-            {
-                echo "ERROR: Missing one of source files for customer {$cust->getShortname()} on switch {$switch}.\n";
                 continue;
-            }
             
-            $command = "./graph-sync.php -m $mrtg -r $rrd -o $out";
-            @exec( $command );   
+            $command = "./graph-sync.php -m " . escapeshellarg( $mrtg ) . " -r " . escapeshellarg( $rrd ) . " -o " . escapeshellarg( $out );
+            //@exec( $command );   
+            echo "$command\n";
+            
+            $i++;
+            
+            if( $i % 10 == 0 )
+                echo sprintf( "echo '---> %0.1f%%'\n", ($i / $count) * 100 );
         }
     }
 }
