e18e998fcc8acde22f22a11286c074999ccd06cd
Barry O'Donovan <barry@opensolutions.ie>
2013-08-01 09:54:09 +0100

    Fix merge conflicts

--- library/IXP/Version.php
+++ library/IXP/Version.php
@@ -3,11 +3,6 @@
 # Basic version information
 #
 
-<<<<<<< HEAD
 define( 'APPLICATION_VERSION', '3.4.0' );
 define( 'APPLICATION_VERDATE', '2013xxxxxxxxxx' );
-=======
-define( 'APPLICATION_VERSION', '3.3.4' );
-define( 'APPLICATION_VERDATE', '20130801094900' );
->>>>>>> master
 
