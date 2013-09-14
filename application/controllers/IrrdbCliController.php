c7d6d7af62e811cad835c77d3b5921b66552f74d
Barry O'Donovan <barry@opensolutions.ie>
2013-09-14 16:38:38 +0100

    [IM] Complete yesterday's optimisation work

--- application/controllers/IrrdbCliController.php
+++ application/controllers/IrrdbCliController.php
@@ -68,9 +68,9 @@ class IrrdbCliController extends IXP_Controller_CliAction
                     $prefixes = $bgpq3->getPrefixList( $asmacro, $protocol );
                     $this->netTime += ( microtime( true ) - $timing );
                     
-                    $this->verbose( "found " . count( $prefixes ), false );
+                    $this->verbose( "IRRDB " . count( $prefixes ), false );
                     
-                    if( $this->updateCustomerPrefixes( $c, $prefixes, $protocol ) )
+                    if( $this->updateCustomerPrefixesViaArray( $c, $prefixes, $protocol ) )
                         $this->verbose( "; DB updated] ", false );
                     else
                         $this->verbose( "; DB not updated] ", false );
@@ -86,9 +86,9 @@ class IrrdbCliController extends IXP_Controller_CliAction
             $this->verbose();
         }
         
-        $this->debug( "Database time  : " . $this->dbTime );
-        $this->debug( "Processing time: " . $this->procTime );
-        $this->debug( "Network time   : " . $this->netTime );
+        $this->verbose( "Database time  : " . $this->dbTime );
+        $this->verbose( "Processing time: " . $this->procTime );
+        $this->verbose( "Network time   : " . $this->netTime );
     }
 
     /**
@@ -104,17 +104,16 @@ class IrrdbCliController extends IXP_Controller_CliAction
      *
      * The above is all one transaction ensuring the member's prefixes are available to any script requiring them.
      *
+     * This function allows the database to do the heavy lifting and it's timings break down as follows:
+     *
+     *
      * @param \Entities\Customer $cust The customer to update the prefixes of
      * @param array $prefixes An array of prefixes
      * @param int $protocol The protocol to use (4 or 6)
      * @throws Exception
      */
-    private function updateCustomerPrefixes( $cust, $prefixes, $protocol )
+    private function updateCustomerPrefixesViaDatabase( $cust, $prefixes, $protocol )
     {
-        $conn = $this->getD2EM()->getConnection();
-        
-        $timing = microtime( true );
-        
         // The calling function and the IXP_BGPQ3 class does a lot of validation and error
         // checking. But the last thing we need to do is start filtering all prefixes if
         // something falls through to here. So, as a basic check, make sure we do not accept
@@ -122,6 +121,7 @@ class IrrdbCliController extends IXP_Controller_CliAction
         
         if( count( $prefixes ) == 0 )
         {
+            $timing = microtime( true );
             // make sure the customer doesn't have a non-empty prefix set that we're about to delete
             if( $this->getD2R( '\\Entities\\IrrdbPrefix' )->getCountForCustomerAndProtocol( $cust, $protocol ) != 0 )
             {
@@ -137,7 +137,14 @@ class IrrdbCliController extends IXP_Controller_CliAction
             return false;
         }
         
+        // validate IRRDB prefixes before we put them near the database
         
+        $timing = microtime( true );
+        $prefixes = $this->validatePrefixes( $prefixes, $protocol );
+        $this->procTime += ( microtime( true ) - $timing );
+        
+        $timing = microtime( true );
+        $conn = $this->getD2EM()->getConnection();
         $conn->beginTransaction();
         
         try
@@ -186,15 +193,8 @@ class IrrdbCliController extends IXP_Controller_CliAction
     /**
      * Update the database IrrdbPrefix table with the member's prefixes for a given protocol.
      *
-     * This is transaction safe and works as follows:
-     *
-     * * Record the current time and use for last seen stamps
-     * * For each prefix:
-     *   * try to UPDATE a row of the same details stamping a last seen time
-     *   * if UPDATE fails, INSERT instead
-     * * delete any prefixes for this customer and protocol with a last seen before the recorded current time.
-     *
-     * The above is all one transaction ensuring the member's prefixes are available to any script requiring them.
+     * This is transaction safe and works as follows ensuring the member's prefixes are available
+     * to any script requiring them at any time.
      *
      * @param \Entities\Customer $cust The customer to update the prefixes of
      * @param array $prefixes An array of prefixes
@@ -204,7 +204,6 @@ class IrrdbCliController extends IXP_Controller_CliAction
     private function updateCustomerPrefixesViaArray( $cust, $prefixes, $protocol )
     {
         $conn = $this->getD2EM()->getConnection();
-        
         $timing = microtime( true );
         $dbPrefixes = $this->getD2R( '\\Entities\\IrrdbPrefix' )->getForCustomerAndProtocol( $cust, $protocol );
         $this->dbTime += ( microtime( true ) - $timing );
@@ -233,20 +232,25 @@ class IrrdbCliController extends IXP_Controller_CliAction
         
         foreach( $dbPrefixes as $i => $p )
         {
-            if( ( $i2 = array_search( $p, $prefixes ) ) !== false )
+            if( ( $j = array_search( $p['prefix'], $prefixes ) ) !== false )
             {
                 // prefix exists in both db and IRRDB - no action required
                 unset( $dbPrefixes[ $i ] );
-                unset( $prefixes[ $i2 ] );
+                unset( $prefixes[ $j ] );
             }
         }
-        
-        $this->procTime += ( microtime( true ) - $timing );
         
         // at this stage, the arrays are now:
         // $dbPrefixes => prefixes in the database that need to be deleted
         // $prefixes   => new prefixes that need to be added
         
+        $this->verbose( "; " . count( $dbPrefixes ) . " stale", false );
+        $this->verbose( "; " . count( $prefixes ) . " new", false );
+        
+        // validate any remaining IRRDB prefixes before we put them near the database
+        $prefixes = $this->validatePrefixes( $prefixes, $protocol );
+        
+        $this->procTime += ( microtime( true ) - $timing );
         
         $timing = microtime( true );
         $conn->beginTransaction();
@@ -255,23 +259,28 @@ class IrrdbCliController extends IXP_Controller_CliAction
         {
             foreach( $prefixes as $prefix )
             {
+                $this->debug( "INSERT: {$cust->getShortname()} IPv{$protocol} {$prefix}", false );
+                $now = date( 'Y-m-d H:i:s' );
                 $conn->executeUpdate(
                     "INSERT INTO irrdb_prefix ( customer_id, prefix, protocol, last_seen, first_seen ) VALUES ( ?, ?, ?, ?, ? )",
-                    [ $cust->getId(), $prefix, $protocol, NOW(), NOW() ]
+                    [ $cust->getId(), $prefix, $protocol, $now, $now ]
                 );
+                $this->debug( ' - DONE.' );
             }
             
             foreach( $dbPrefixes as $prefix )
             {
+                $this->debug( "DELETE: {$cust->getShortname()} IPv{$protocol} {$prefix['prefix']}", false );
                 $conn->executeUpdate(
                     "DELETE FROM irrdb_prefix WHERE customer_id = ? AND protocol = ? AND prefix = ?",
-                    [ $cust->getId(), $protocol, $prefix ]
+                    [ $cust->getId(), $protocol, $prefix['prefix'] ]
                 );
+                $this->debug( ' - DONE.' );
             }
             
             $conn->executeUpdate(
-                "UPDATE irrdb_prefix SET last_seen = NOW() WHERE customer_id = ? AND protocol = ?",
-                [ $cust->getId(), $protocol ]
+                "UPDATE irrdb_prefix SET last_seen = ? WHERE customer_id = ? AND protocol = ?",
+                [ date( 'Y-m-d H:i:s' ), $cust->getId(), $protocol ]
             );
                 
             $conn->commit();
@@ -286,6 +295,31 @@ class IrrdbCliController extends IXP_Controller_CliAction
         }
         
         return true;
+    }
+    
+    /**
+     * Validate a given array of CIDR formatted prefixes for the given protocol and
+     * remove (and alert on) any elements failing validation.
+     *
+     * @param array $prefixes Prefixes in CIDR notation
+     * @param int $protocol Either 4/6
+     * @return array Valid prefixes
+     */
+    private function validatePrefixes( $prefixes, $protocol )
+    {
+        $fn = "OSS_Validate_OSSIPv{$protocol}Cidr";
+        $validator = new $fn();
+        
+        foreach( $prefixes as $i => $p )
+        {
+            if( !$validator->isValid( $p ) )
+            {
+                unset( $prefixes[$i] );
+                $this->getLogger()->alert( 'IRRDB CLI action - removing invalid prefix ' . $p . ' from IRRDB result set!' );
+            }
+        }
+        
+        return $prefixes;
     }
 }
 
