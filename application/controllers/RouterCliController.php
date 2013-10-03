fcc4f05be3efb0d9cd1065e9ec18ff9d16ddadfe
Barry O'Donovan <barry@opensolutions.ie>
2013-10-03 11:53:53 +0100

    [NF] Route server testing framework
    See: https://github.com/inex/IXP-Manager/wiki/Route-Server-Testing

--- application/controllers/RouterCliController.php
+++ application/controllers/RouterCliController.php
@@ -92,8 +92,6 @@ class RouterCliController extends IXP_Controller_CliAction
                 : false
         );
     
-        $this->collectorConfSanityCheck( $vlan );
-    
         $this->view->proto = $proto = $this->cliResolveProtocol( false );
     
         if( $proto == 6 )
@@ -107,10 +105,16 @@ class RouterCliController extends IXP_Controller_CliAction
         // should we limit this to one customer only?
         $lcustomer = $this->cliResolveParam( 'cust', false, false );
         
+        // should we wrap the output with the header and footer
+        $wrappers = (bool)$this->cliResolveParam( 'wrappers', false, true );
+
+        // is test mode enabled?
+        $this->view->testmode = (bool)$this->cliResolveParam( 'testmode', false, false );
+
         // load Smary config file
         $this->getView()->configLoad( $this->loadConfig() );
         
-        if( !$lcustomer && $this->getView()->templateExists( "router-cli/server/{$target}/header.cfg" ) )
+        if( !$lcustomer && $wrappers && $this->getView()->templateExists( "router-cli/server/{$target}/header.cfg" ) )
             echo $this->view->render( "router-cli/server/{$target}/header.cfg" );
         
         foreach( $ints as $int )
@@ -124,10 +128,140 @@ class RouterCliController extends IXP_Controller_CliAction
             echo $this->view->render( "router-cli/server/{$target}/neighbor.cfg" );
         }
         
-        if( !$lcustomer && $this->getView()->templateExists( "router-cli/server/{$target}/footer.cfg" ) )
+        if( !$lcustomer && $wrappers && $this->getView()->templateExists( "router-cli/server/{$target}/footer.cfg" ) )
             echo $this->view->render( "router-cli/server/{$target}/footer.cfg" );
     }
     
+    /**
+     * Action to generate test route server client configurations
+     *
+     * @see https://github.com/inex/IXP-Manager/wiki/Route-Server-Testing
+     */
+    public function genServerTestConfsAction()
+    {
+        $this->view->vlan = $vlan = $this->cliResolveVlanId();
+    
+        $target = $this->cliResolveTarget(
+                isset( $this->_options['router']['collector']['conf']['target'] )
+                ? $this->_options['router']['collector']['conf']['target']
+                : false
+        );
+    
+        $this->view->proto = $proto = $this->cliResolveProtocol( false );
+    
+        if( $proto == 6 )
+            $ints = $this->sanitiseVlanInterfaces( $vlan, 6, true );
+        else
+        {
+            $ints = $this->sanitiseVlanInterfaces( $vlan, 4, true );
+            $this->view->proto = $proto = 4;
+        }
+
+        // prepare the test directory and its subdirectories
+        $dir = $this->prepareTestDirectory();
+        
+        // should we limit this to one customer only?
+        $lcustomer = $this->cliResolveParam( 'cust', false, false );
+    
+        // load Smary config file
+        $this->getView()->configLoad( $this->loadConfig() );
+    
+        foreach( $ints as $int )
+        {
+            if( $lcustomer && $int['cshortname'] != $lcustomer )
+                continue;
+    
+            $this->view->int  = $int;
+            $this->view->prefixes = $this->getD2R( '\\Entities\\IrrdbPrefix' )->getForCustomerAndProtocol( $int[ 'cid' ], $proto );
+            file_put_contents( "{$dir}/confs/{$int['cshortname']}-vlanid{$vlan->getId()}-ipv{$proto}.conf", $this->view->render( "router-cli/server-testing/{$target}.cfg" ) );
+        }
+    }
+
+    /**
+     * Action to generate test route server client setup commands
+     *
+     * @see https://github.com/inex/IXP-Manager/wiki/Route-Server-Testing
+     */
+    public function genServerTestSetupAction()
+    {
+        $this->view->vlan = $vlan = $this->cliResolveVlanId();
+    
+        $target = $this->cliResolveTarget(
+                isset( $this->_options['router']['collector']['conf']['target'] )
+                ? $this->_options['router']['collector']['conf']['target']
+                : false
+        );
+    
+        $this->view->proto = $proto = $this->cliResolveProtocol( false );
+    
+        if( $proto == 6 )
+            $ints = $this->sanitiseVlanInterfaces( $vlan, 6, true );
+        else
+        {
+            $ints = $this->sanitiseVlanInterfaces( $vlan, 4, true );
+            $this->view->proto = $proto = 4;
+        }
+
+        if( $vlan->getSubnetSize( $proto ) === false )
+            throw new IXP_Exception( "Subnet size for this VLAN is not defined. See http://git.io/TkSVVw" );
+        
+        // should we limit this to one customer only?
+        $lcustomer = $this->cliResolveParam( 'cust', false, false );
+    
+        // prepare the test directory and its subdirectories
+        $this->view->dir = $this->prepareTestDirectory();
+        
+        // the OS to generate commands for
+        $os = $this->cliResolveParam( 'os', false, 'linux' );
+        
+        // generate down rather than up commands?
+        $down = $this->cliResolveParam( 'down', false, false );
+        
+        // load Smary config file
+        $this->getView()->configLoad( $this->loadConfig() );
+    
+        foreach( $ints as $int )
+        {
+            if( $lcustomer && $int['cshortname'] != $lcustomer )
+                continue;
+    
+            $this->view->int  = $int;
+            if( $down )
+                echo $this->view->render( "router-cli/server-testing/{$target}-{$os}-setup-down.cfg" );
+            else
+                echo $this->view->render( "router-cli/server-testing/{$target}-{$os}-setup-up.cfg" );
+        }
+    }
+    
+    /**
+     * Used by the route server test generator to create and prepare a test
+     * directory.
+     *
+     * @see genServerTestConfsAction()
+     */
+    private function prepareTestDirectory()
+    {
+        // we need a directory to spit out the files
+        $dir = realpath( $this->cliResolveParam( 'dir', true ) );
+
+        if( !$dir )
+            throw new IXP_Exception( "The target directory does not exist" );
+        else if( !is_dir( $dir ) )
+            throw new IXP_Exception( "{$dir} exists but is not a directory" );
+        else if( !is_writable( $dir ) )
+            throw new IXP_Exception( "{$dir} is not writable" );
+        
+        if( !file_exists( "{$dir}/confs" ) )
+        {
+            if( !mkdir( "{$dir}/confs" ) )
+            {
+                echo "ERROR: {$dir}/confs does not exist and could not be created\n";
+                return;
+            }
+        }
+        
+        return $dir;
+    }
     
     /**
      * Action to generate an AS112 router configuration
