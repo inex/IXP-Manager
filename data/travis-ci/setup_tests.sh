1ba34f7e08689d64c628c68da3b20acad46e09b3
Barry O'Donovan <barry@opensolutions.ie>
2014-01-06 12:00:55 +0000

    [CI] Improve setup script

--- data/travis-ci/setup_tests.sh
+++ data/travis-ci/setup_tests.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # install requirements
-sudo apt-get install php5-memcache
-sudo apt-get install php5-snmp
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install php5-memcache php5-snmp php-pear apache2 libapache2-mod-php5
 
 # install Doctrine ORM
-sudo apt-get install php-pear
 sudo pear channel-discover pear.symfony.com
 sudo pear channel-discover pear.doctrine-project.org
+# no non-interactive option: sudo pear upgrade-all
 sudo pear install doctrine/DoctrineORM
 cd /usr/share/php/Doctrine
 sudo ln -s ../Symfony
 cd -
 
-# install Apache
-sudo apt-get install apache2
+# setup Apache
 sudo a2enmod rewrite
 
