2fa5b5348a0e58ec49058b02ed5dfc230974c53f
Barry O'Donovan <barry@opensolutions.ie>
2014-02-17 20:36:14 +0000

    Version bump

--- library/IXP/Version.php
+++ library/IXP/Version.php
@@ -3,6 +3,6 @@
 # Basic version information
 #
 
-define( 'APPLICATION_VERSION', '3.6.6' );
-define( 'APPLICATION_VERDATE', '20140203153500' );
+define( 'APPLICATION_VERSION', '3.6.8' );
+define( 'APPLICATION_VERDATE', '20140217203500' );
 
