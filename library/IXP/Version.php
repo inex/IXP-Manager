a1ee5ef2d7b511493419db441e205b3eabecb5c0
Barry O'Donovan <barry@opensolutions.ie>
2014-02-17 21:05:34 +0000

    Version bump due to bug in v3.6.8

--- library/IXP/Version.php
+++ library/IXP/Version.php
@@ -3,6 +3,6 @@
 # Basic version information
 #
 
-define( 'APPLICATION_VERSION', '3.6.8' );
-define( 'APPLICATION_VERDATE', '20140217203500' );
+define( 'APPLICATION_VERSION', '3.6.9' );
+define( 'APPLICATION_VERDATE', '20140217210500' );
 
