be98af50171d7b58c5c20c598ec989e80fb4afbe
Barry O'Donovan <barry@opensolutions.ie>
2014-08-24 13:01:01 +0100

    [IM] Do not hardcode configuration name - closes #162

--- tools/runtime/route-servers/api-reconfigure-example.sh
+++ tools/runtime/route-servers/api-reconfigure-example.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Example script used by INEX to regularly (or on demand) rebuild and 
+# Example script used by INEX to regularly (or on demand) rebuild and
 # reconfigure our Bird route server configuration.
 #
 # Barry O'Donovan <barry@opensolutions.ie>
@@ -13,13 +13,14 @@ URL="https://www.ixp.com/apiv1/router/server-conf/key/${KEY}"
 ETCPATH="/usr/local/etc/bird"
 RUNPATH="/var/run/bird"
 BIN="/usr/sbin/bird"
+CONFNAME="rs2"
 
 # Parse arguments
 export DEBUG=0
 
 function show_help {
     echo "$0 [-d] -v <vlan id> -p <protocol 4/6> [-h|-?]"
-}                                                            
+}
 
 
 while getopts "h?qdv:p:" opt; do
@@ -41,7 +42,7 @@ if [[ -z "$vlanid" ]]; then
     echo ERROR: VLAN ID parameter -v is required
     exit 1
 fi
-                                                            
+
 if [[ -z "$proto" ]]; then
     echo ERROR: Protocol parameter -p is required
     exit 1
@@ -58,13 +59,13 @@ else
 fi
 
 dest="${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf.$$"
-        
+
 cmd="wget -q -O $dest \
-    \"${URL}/target/bird/vlanid/${vlanid}/proto/${proto}/config/rs2-vlanid${vlanid}-ipv${proto}\""
-            
+    \"${URL}/target/bird/vlanid/${vlanid}/proto/${proto}/config/${CONFNAME}-vlanid${vlanid}-ipv${proto}\""
+
 if [[ $DEBUG -eq 1 ]]; then echo $cmd; fi
 eval $cmd
-        
+
 # We want to be bullet proof here so we really want to check the generated file to try
 # and ensure it is valid
 
@@ -76,7 +77,7 @@ fi
 if [[ ! -e $dest || ! -s $dest ]]; then
     echo "ERROR: $dest does not exist or is zero size"
     exit 3
-fi 
+fi
 
 if [[ $( cat $dest | grep "protocol bgp pb_" | wc -l ) -lt 2 ]]; then
     echo "ERROR: <2 BGP protocol defintions in config file $dest - something has gone wrong..."
@@ -91,13 +92,13 @@ if [[ $? -ne 0 ]]; then
     echo "ERROR: non-zero return from bird${PROTOCOL} when parsing $dest"
     exit 7
 fi
-        
+
 # config file should be okay; back up the current one
 if [[ -e ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf ]]; then
     cp "${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf" "${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf.old"
 fi
 mv $dest ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf
-            
+
 # are we running or do we need to be started?
 cmd="${BIN}c${PROTOCOL} -s ${RUNPATH}/bird-vlanid${vlanid}-ipv${proto}.ctl show memory"
 if [[ $DEBUG -eq 1 ]]; then echo $cmd; fi
@@ -105,10 +106,10 @@ eval $cmd &>/dev/null
 
 if [[ $? -ne 0 ]]; then
     cmd="${BIN}${PROTOCOL} -c ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf -s ${RUNPATH}/bird-vlanid${vlanid}-ipv${proto}.ctl"
-            
+
     if [[ $DEBUG -eq 1 ]]; then echo $cmd; fi
     eval $cmd &>/dev/null
-                                    
+
     if [[ $? -ne 0 ]]; then
         echo "ERROR: bird{$PROTOCOL} was not running for $dest and could not be started"
         exit 5
@@ -120,7 +121,7 @@ else
 
     if [[ $? -ne 0 ]]; then
         echo "ERROR: Reconfigure failed for $dest"
-        
+
         if [[ -e ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf.old ]]; then
             echo "Trying to revert to previous"
             mv ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf $dest
@@ -133,10 +134,10 @@ else
             else
                 echo Reversion failed
                 exit 6
-            fi 
+            fi
         fi
     fi
-    
+
 fi
 
 exit 0
