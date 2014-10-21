84ee9cb20334188a7563b6f25053e0e53e17eaa5
Barry O'Donovan <barry@opensolutions.ie>
2014-10-21 09:02:44 +0100

    Tag new release v3.6.17

--- library/IXP/Version.php
+++ library/IXP/Version.php
@@ -3,6 +3,6 @@
 # Basic version information
 #
 
-define( 'APPLICATION_VERSION', '3.6.16' );
-define( 'APPLICATION_VERDATE', '20140824140300' );
+define( 'APPLICATION_VERSION', '3.6.17' );
+define( 'APPLICATION_VERDATE', '20141021090200' );
 
