36740067a6ce991867d22ab2cf0b62da1dd90da8
Barry O'Donovan <barry@opensolutions.ie>
2015-02-18 11:12:11 +0000

    Fixes #200 - checking on an undefined snmpid, do not include those into the mrtg conf

--- application/controllers/StatisticsCliController.php
+++ application/controllers/StatisticsCliController.php
@@ -480,18 +480,21 @@ class StatisticsCliController extends IXP_Controller_CliAction
 
                 foreach( $switch->getPorts() as $port )
                 {
-                    $snmpId = $port->ifnameToSNMPIdentifier();
-                    $data[ $infra->getId() ]['maxbytes'] += $port->getIfHighSpeed() * 1000000 / 8; // Mbps * bps / to bytes
-                    $data[ $infra->getId() ]['switches'][ $switch->getId() ]['maxbytes'] += $port->getIfHighSpeed() * 1000000 / 8;
-
-                    foreach( IXP_Mrtg::$TRAFFIC_TYPES as $type => $vars )
+                    if( $port->getIfName() )
                     {
-                        $id = "{$vars['in']}#{$snmpId}&{$vars['out']}#{$snmpId}:{$switch->getSnmppasswd()}@{$switch->getHostname()}:::::2";
-
-                        if( $port->getType() == \Entities\SwitchPort::TYPE_PEERING )
-                            $data[ $infra->getId() ]['mrtgIds'][$type][] = $id;
-
-                        $data[ $infra->getId() ]['switches'][ $switch->getId() ]['mrtgIds'][$type][] = $id;
+                        $snmpId = $port->ifnameToSNMPIdentifier();
+                        $data[ $infra->getId() ]['maxbytes'] += $port->getIfHighSpeed() * 1000000 / 8; // Mbps * bps / to bytes
+                        $data[ $infra->getId() ]['switches'][ $switch->getId() ]['maxbytes'] += $port->getIfHighSpeed() * 1000000 / 8;
+    
+                        foreach( IXP_Mrtg::$TRAFFIC_TYPES as $type => $vars )
+                        {
+                            $id = "{$vars['in']}#{$snmpId}&{$vars['out']}#{$snmpId}:{$switch->getSnmppasswd()}@{$switch->getHostname()}:::::2";
+    
+                            if( $port->getType() == \Entities\SwitchPort::TYPE_PEERING )
+                                $data[ $infra->getId() ]['mrtgIds'][$type][] = $id;
+    
+                            $data[ $infra->getId() ]['switches'][ $switch->getId() ]['mrtgIds'][$type][] = $id;
+                        }
                     }
                 }
             }
