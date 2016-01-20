71717f9f630a6f061896215af98714e0ec27c795
Barry O'Donovan <barry@opensolutions.ie>
2016-01-20 20:49:15 +0000

    Changes from live

--- app/Console/Commands/Helpdesk/UpdateOrganisations.php
+++ app/Console/Commands/Helpdesk/UpdateOrganisations.php
@@ -141,8 +141,6 @@ class UpdateOrganisations extends HelpdeskCommand {
         {
             if( $this->getHelpdesk()->contactNeedsUpdating( $contact, $user ) )
             {
-            this->error( "{$this->name} :: could not update {$contact->getName()} :: FIXME - UPDATES DISABLED" );
-            return; //FIXME!!!!!!
                 if( $this->getHelpdesk()->userUpdate( $user->helpdesk_id, $contact ) )
                     $this->info( "{$this->name} :: updated {$contact->getName()}" );
                 else
@@ -167,8 +165,11 @@ class UpdateOrganisations extends HelpdeskCommand {
             }
             catch ( ApiException $e )
             {
-                if( $this->getHelpdesk()->getDebug() && $this->getHelpdesk()->getDebug()->lastResponseCode == 422 )
+                if( $e->userIsDuplicateEmail() ) {
+                    if( $this->getOutput()->isVerbose() )
+                        $this->info( "{$this->name} :: skipping contact {$contact->getName()}/{$contact->getId()} as email is a duplicate on helpdesk..." );
                     return;
+                }
 
                 throw $e;
             }
