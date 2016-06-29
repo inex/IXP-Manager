02c6872583e33828633a3ad18eaee919eda83620
Barry O'Donovan <barry@opensolutions.ie>
2016-06-29 20:00:28 +0100

    Move legacy traffic CLI stats to Laravel config

--- app/Providers/ZendFrameworkServiceProvider.php
+++ app/Providers/ZendFrameworkServiceProvider.php
@@ -64,6 +64,7 @@ class ZendFrameworkServiceProvider extends ServiceProvider {
         $options = $this->setupIxpTools($options);
         $options = $this->setupDisabledFrontendControllers($options);
         $options = $this->setupMailingLists($options);
+        $options = $this->setupGrapherCli($options);
 
         // now we need to shove these options back into ZendFramework.
         // There's a but of duplication and complexity here:
@@ -314,6 +315,33 @@ class ZendFrameworkServiceProvider extends ServiceProvider {
                     $options['weathermap'][$id][$k]  = $v;
                 }
             }
+        }
+
+        return $options;
+    }
+
+
+    /**
+     * Setup legacy Grapher CLI tools.
+     */
+    private function setupGrapherCli( array $options ): array {
+
+        if( is_array( config('grapher_cli.cli') ) ) {
+            foreach( config('grapher_cli.cli') as $fn => $details ) {
+                foreach( $details as $k => $v ) {
+                    if( $k == 'recipients' ) {
+                        foreach( $details[$k] as $r ) {
+                            $options['cli'][$fn][$k][] = $r;
+                        }
+                    } else {
+                        $options['cli'][$fn][$k]  = $v;
+                    }
+                }
+            }
+        }
+        
+        if( config( 'grapher_cli.cli.traffic_daily.delete_old' ) ) {
+            $options['cli']['traffic_daily']['delete_old'] = config( 'grapher_cli.cli.traffic_daily.delete_old' );
         }
 
         return $options;
