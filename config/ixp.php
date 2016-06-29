845e0e993bfdd0150d00ca4a7dd0e2875cc24e09
Barry O'Donovan <barry@opensolutions.ie>
2016-06-29 11:39:36 +0100

    These configuration options were previously Laravel-ified

--- config/ixp.php
+++ config/ixp.php
@@ -12,10 +12,8 @@ return [
     ;;
     ;; See: https://github.com/inex/IXP-Manager/wiki/Multi-IXP-Functionality
     ;; */
-    [
-        'multiixp' => [
-            'enabled' => env( 'IXP_MULTIIXP_ENABLED', false ),
-        ]
+    'multiixp' => [
+        'enabled' => env( 'IXP_MULTIIXP_ENABLED', false ),
     ],
 
     /* ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -24,14 +22,12 @@ return [
     ;;
     ;; See: https://github.com/inex/IXP-Manager/wiki/Reseller-Functionality
     ;; */
-    [
-        'reseller' => [
-            'enabled' => env( 'IXP_RESELLER_ENABLED', false ),
+    'reseller' => [
+        'enabled' => env( 'IXP_RESELLER_ENABLED', false ),
 
-            // If reseller mode enabled and this is set to true then super admin or customer itself
-            // can not add/change resold customers details.
-            'reseller' => env( 'IXP_RESELLER_RESOLD_BILLING', false ),
-        ],
+        // If reseller mode enabled and this is set to true then super admin or customer itself
+        // can not add/change resold customers details.
+        'reseller' => env( 'IXP_RESELLER_RESOLD_BILLING', false ),
     ],
 
 
@@ -40,10 +36,8 @@ return [
     ;;
     ;; See https://github.com/inex/IXP-Manager/wiki/AS112
     ;; */
-    [
-        'as112' => [
-            'ui_active' => env( 'IXP_AS112_UI_ACTIVE', false ),
-        ],
+    'as112' => [
+        'ui_active' => env( 'IXP_AS112_UI_ACTIVE', false ),
     ],
 
 ];
