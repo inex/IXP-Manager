78a5bff61ca1552b7a73e03dc0f13d84eaf5b755
Nick Hilliard <nick@foobar.org>
2016-09-23 18:14:29 +0100

    Update copyright statement and redistribution rights

--- tools/runtime/route-collectors/build-tt-member-configuration.pl
+++ tools/runtime/route-collectors/build-tt-member-configuration.pl
@@ -1,14 +1,23 @@
 #!/usr/bin/perl
 #
-# Copyright Internet Neutral Exchange Association Limited (INEX) 2008.
+# Copyright (C) 2008-2016 Internet Neutral Exchange Association Company Limited By Guarantee.
 # All Rights Reserved.
 #
-# Author: Nick Hilliard <nick@inex.ie>
+# This file is part of IXP Manager.
 #
-# Redistribution of this file is prohibited without prior written permission
-# from INEX. 
+# IXP Manager is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version v2.0 of the License.
 #
-# $Id: build-tt-member-configuration.pl,v 1.10 2010/02/03 13:45:43 nick Exp $
+# IXP Manager is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License v2.0
+# along with IXP Manager.  If not, see:
+#
+# http://www.gnu.org/licenses/gpl-2.0.html
 #
 
 use warnings;
