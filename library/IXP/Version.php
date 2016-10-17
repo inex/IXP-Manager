2686124d5c6cab40769c2f7a818ae559ae346c83
Barry O'Donovan <barry@opensolutions.ie>
2016-10-17 19:40:06 +0100

    Version bump 3.7.4 -> 3.7.5
    Looks like 3.7.4 was stamped previously without an update to Version.php

--- library/IXP/Version.php
+++ library/IXP/Version.php
@@ -3,5 +3,5 @@
 # Basic version information
 #
 
-define( 'APPLICATION_VERSION', '3.7.4' );
-define( 'APPLICATION_VERDATE', '20161017193700' );
+define( 'APPLICATION_VERSION', '3.7.5' );
+define( 'APPLICATION_VERDATE', '20161017193900' );
