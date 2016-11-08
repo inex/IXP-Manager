59d2b9f6937f8502d1432bdd191cf1cad8ab3064
Barry O'Donovan <barry@opensolutions.ie>
2016-11-08 15:11:12 +0000

    INEX custom conf for as112

---
+++ resources/skins/inex/api/v4/router/as112/bird/custom-neighbors.foil.php
@@ -0,0 +1,57 @@
+<?php
+
+switch( $t->router->handle() ) {
+    case 'as112-lan1-ipv4':
+        $rs1 = '185.6.36.8';
+        $rs2 = '185.6.36.9';
+        $rc1 = '185.6.36.126';
+        break;
+
+    case 'as112-lan1-ipv6':
+        $rs1 = '2001:7f8:18::8';
+        $rs2 = '2001:7f8:18::9';
+        $rc1 = '2001:7f8:18::f:0:1';
+        break;
+
+    case 'as112-lan2-ipv4':
+        $rs1 = '194.88.240.8';
+        $rs2 = '194.88.240.9';
+        $rc1 = '194.88.240.126';
+        break;
+
+    case 'as112-lan2-ipv6':
+        $rs1 = '2001:7f8:18:12::8';
+        $rs2 = '2001:7f8:18:12::9';
+        $rc1 = '2001:7f8:18:12::9999';
+        break;
+}
+
+?>
+
+
+protocol bgp pb_as2128_ipv<?= $t->router->protocol() ?> {
+        description "AS2128 - INEX Router Collector";
+        local as routerasn;
+        source address routeraddress;
+        neighbor <?= $rc1 ?> as 2128;
+        import filter f_import_policy;
+        export where proto = "static_as112";
+}
+
+protocol bgp pb_as43760_1_ipv<?= $t->router->protocol() ?> {
+        description "AS43760 - INEX Route Server #1";
+        local as routerasn;
+        source address routeraddress;
+        neighbor <?= $rs1 ?> as 43760;
+        import filter f_import_policy;
+        export where proto = "static_as112";
+}
+
+protocol bgp pb_as43760_2_ipv<?= $t->router->protocol() ?> {
+        description "AS43760 - INEX Route Server #2";
+        local as routerasn;
+        source address routeraddress;
+        neighbor <?= $rs2 ?> as 43760;
+        import filter f_import_policy;
+        export where proto = "static_as112";
+}
