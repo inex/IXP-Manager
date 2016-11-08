2e26081b6f597385a07bc3147ada37746b6bfa76
Barry O'Donovan <barry@opensolutions.ie>
2016-11-08 15:08:07 +0000

    [NF] Templates for AS112 service

---
+++ resources/views/api/v4/router/as112/bird/neighbors.foil.php
@@ -0,0 +1,23 @@
+
+<?php foreach( $t->ints as $int ):
+    
+        // do not set up a session to ourselves!
+        if( $int['autsys'] == $t->router->asn() ):
+            continue;
+        endif;
+
+?>
+
+protocol bgp pb_as<?= $int['autsys'] ?>_vli<?= $int['vliid'] ?>_ipv<?= $int['protocol'] ?? 4 ?> {
+        description "AS<?= $int['autsys'] ?> - <?= $int['cname'] ?>";
+        local as routerasn;
+        source address routeraddress;
+        neighbor <?= $int['address'] ?> as <?= $int['autsys'] ?>;
+        import filter f_import_policy;
+        export where proto = "static_as112";
+        import limit <?= $int['maxprefixes'] ?>;
+        <?php if( $int['bgpmd5secret'] ): ?>password "<?= $int['bgpmd5secret'] ?>";<?php endif; ?>
+
+}
+
+<?php endforeach; ?>
