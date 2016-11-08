cd40af4eae1d64df1406f7a09d2b112a8538aad1
Barry O'Donovan <barry@opensolutions.ie>
2016-11-08 23:24:03 +0000

    Add transit to INEX conf

--- resources/skins/inex/api/v4/router/as112/bird/custom-neighbors.foil.php
+++ resources/skins/inex/api/v4/router/as112/bird/custom-neighbors.foil.php
@@ -2,24 +2,28 @@
 
 switch( $t->router->handle() ) {
     case 'as112-lan1-ipv4':
+        $gw1 = '185.6.36.2';
         $rs1 = '185.6.36.8';
         $rs2 = '185.6.36.9';
         $rc1 = '185.6.36.126';
         break;
 
     case 'as112-lan1-ipv6':
+        $gw1 = '2001:7f8:18::2';
         $rs1 = '2001:7f8:18::8';
         $rs2 = '2001:7f8:18::9';
         $rc1 = '2001:7f8:18::f:0:1';
         break;
 
     case 'as112-lan2-ipv4':
+        $gw1 = '194.88.240.2';
         $rs1 = '194.88.240.8';
         $rs2 = '194.88.240.9';
         $rc1 = '194.88.240.126';
         break;
 
     case 'as112-lan2-ipv6':
+        $gw1 = '2001:7f8:18:12::2';
         $rs1 = '2001:7f8:18:12::8';
         $rs2 = '2001:7f8:18:12::9';
         $rc1 = '2001:7f8:18:12::9999';
@@ -29,13 +33,25 @@ switch( $t->router->handle() ) {
 ?>
 
 
-protocol bgp pb_as2128_ipv<?= $t->router->protocol() ?> {
+protocol bgp pb_as2128_gw1_ipv<?= $t->router->protocol() ?> {
+        description "AS2128 - INEX Transit Connectivity";
+        local as routerasn;
+        source address routeraddress;
+        neighbor <?= $gw1 ?> as 2128;
+        import filter f_import_policy;
+        export where proto = "static_as112";
+        import limit 750000 action restart;
+}
+
+
+protocol bgp pb_as2128_rc1_ipv<?= $t->router->protocol() ?> {
         description "AS2128 - INEX Router Collector";
         local as routerasn;
         source address routeraddress;
         neighbor <?= $rc1 ?> as 2128;
         import filter f_import_policy;
         export where proto = "static_as112";
+        import limit 20 action restart;
 }
 
 protocol bgp pb_as43760_1_ipv<?= $t->router->protocol() ?> {
@@ -45,6 +61,7 @@ protocol bgp pb_as43760_1_ipv<?= $t->router->protocol() ?> {
         neighbor <?= $rs1 ?> as 43760;
         import filter f_import_policy;
         export where proto = "static_as112";
+        import limit 150000 action restart;
 }
 
 protocol bgp pb_as43760_2_ipv<?= $t->router->protocol() ?> {
@@ -54,4 +71,5 @@ protocol bgp pb_as43760_2_ipv<?= $t->router->protocol() ?> {
         neighbor <?= $rs2 ?> as 43760;
         import filter f_import_policy;
         export where proto = "static_as112";
+        import limit 150000 action restart;
 }
