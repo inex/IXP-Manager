19adf386c3c7cbe3401f363e6ea077ed0458d42b
Barry O'Donovan <barry@opensolutions.ie>
2016-11-09 12:02:16 +0100

    Add INEX templates related to last commit

---
+++ resources/skins/inex/header-documentation.foil.php
@@ -0,0 +1,31 @@
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Documentation<b class="caret"></b></a>
+    <ul class="dropdown-menu">
+        <li>
+            <a href="<?= url('/static/fees' ) ?>">Fees and Charges</a>
+        </li>
+        <li>
+            <a href="<?= url('/static/housing' ) ?>">Equipment Housing</a>
+        </li>
+        <li>
+            <a href="<?= url('/static/misc-benefits' ) ?>">Miscellaneous Benefits</a>
+        </li>
+        <li>
+            <a href="<?= url('/static/switches' ) ?>">Connecting Switches</a>
+        </li>
+        <li>
+            <a href="<?= url('/static/port-security' ) ?>">Port Security Policies</a>
+        </li>
+        <li>
+            <a href="<?= url('/static/as112' ) ?>">AS112 Service</a>
+        </li>
+        <li>
+            <a href="<?= url('/static/route-servers' ) ?>">Route Servers</a>
+        </li>
+        
+        <li class="divider"></li>
+        <li>
+            <a href="http://www.ixpmanager.org/" target="_blank">About</a>
+        </li>
+    </ul>
+</li>
