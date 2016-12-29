c3ad6edb57afcb79841a784527ed9c968fa27ecb
Nick Hilliard <nick@foobar.org>
2016-12-29 23:34:26 +0000

    - upgraded route server/collector script to be ixp4 compatible - added gpl

--- tools/runtime/route-servers/api-reconfigure-all.sh
+++ tools/runtime/route-servers/api-reconfigure-all.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
-
+#
+# Copyright (C) 2009-2016 Internet Neutral Exchange Association Company Limited By Guarantee.
+# All Rights Reserved.
+#
+# This file is part of IXP Manager.
+#
+# IXP Manager is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2.0 of the License.
+#
+# IXP Manager is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License v2.0
+# along with IXP Manager.  If not, see:
+#
+# http://www.gnu.org/licenses/gpl-2.0.html
+#
 # Example script used by INEX to rebuild all Bird route servers via API on demand.
 #
-# Barry O'Donovan <barry@opensolutions.ie>
-
-# Edit as appropriate 
+# Author: Barry O'Donovan <barry@opensolutions.ie>
 
 echo "Reconfiguring all bird instances:"
 
-for vlanid in x y z; do
-    echo -ne "VLAN ID ${vlanid}: "
-    for proto in 4 6; do
-        echo -ne "\tIPv${proto}: "
-        /usr/local/sbin/api-reconfigure-example.sh -v $vlanid -p $proto -q
-        if [[ $? -eq 0 ]]; then
-            echo -ne "OK    "
-        else
-            echo -ne "ERROR "
-        fi
-    done
+# These handles should match the definitions in config/routers.php, and
+# should be changed as appropriate:
+
+for handle in rs1-lan1-ipv4 rs1-lan1-ipv6 rs1-lan2-ipv4 rs1-lan2-ipv6; do
+    echo -ne "HANDLE: ${handle}: "
+    /usr/local/sbin/api-reconfigure-example.sh -h $handle -q
+    if [[ $? -eq 0 ]]; then
+        echo -ne "OK    "
+    else
+        echo -ne "ERROR "
+    fi
     echo
 done
-
--- tools/runtime/route-servers/api-reconfigure-example.sh
+++ tools/runtime/route-servers/api-reconfigure-example.sh
@@ -1,76 +1,83 @@
 #! /bin/bash
-
-# Example script used by INEX to regularly (or on demand) rebuild and
-# reconfigure our Bird route server configuration.
 #
-# Barry O'Donovan <barry@opensolutions.ie>
-
-# Edit as appropriate and run as:
-#    api-reconfigure-example.sh -v <vlan id> -p <protocol 4/6>
+# Copyright (C) 2009-2016 Internet Neutral Exchange Association Company Limited By Guarantee.
+# All Rights Reserved.
+#
+# This file is part of IXP Manager.
+#
+# IXP Manager is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2.0 of the License.
+#
+# IXP Manager is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License v2.0
+# along with IXP Manager.  If not, see:
+#
+# http://www.gnu.org/licenses/gpl-2.0.html
+#
 
 KEY="your-api-key"
-URL="https://www.ixp.com/apiv1/router/server-conf/key/${KEY}"
+URL="https://ixp.example.com/ixp/api/v4/router/gen_config"
 ETCPATH="/usr/local/etc/bird"
 RUNPATH="/var/run/bird"
+LOGPATH="/var/log/bird"
 BIN="/usr/sbin/bird"
-CONFNAME="rs2"
 
 # Parse arguments
 export DEBUG=0
 
 function show_help {
-    echo "$0 [-d] -v <vlan id> -p <protocol 4/6> [-h|-?]"
+    echo "$0 [-d] -h <handle> [-?]"
 }
 
 
-while getopts "h?qdv:p:" opt; do
+while getopts "?qdh:" opt; do
     case "$opt" in
-        h|\?)
+        \?)
             show_help
             exit 0
             ;;
         d)  export DEBUG=1
             ;;
-        v)  vlanid=$OPTARG
-            ;;
-        p)  proto=$OPTARG
+        h)  handle=$OPTARG
             ;;
     esac
 done
 
-if [[ -z "$vlanid" ]]; then
-    echo ERROR: VLAN ID parameter -v is required
+if [[ -z "$handle" ]]; then
+    echo ERROR: handle is required
     exit 1
 fi
 
-if [[ -z "$proto" ]]; then
-    echo ERROR: Protocol parameter -p is required
-    exit 1
-fi
-
-mkdir -p $RUNPATH
 mkdir -p $ETCPATH
+mkdir -p $LOGPATH
+mkdir -p $RUNPATH
 
-# to generate the appropriate bird commands:
-if [[ $proto = "6" ]]; then
+# generate the appropriate bird commands:
+if [[ $handle == *ipv6 ]]; then
     PROTOCOL="6"
 else
     PROTOCOL=""
 fi
 
-dest="${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf.$$"
+cfile="${ETCPATH}/bird-${handle}.conf"
+dest="${cfile}.$$"
+socket="${RUNPATH}/bird-${handle}.ctl"
 
-cmd="wget -q -O $dest \
-    \"${URL}/target/bird/vlanid/${vlanid}/proto/${proto}/config/${CONFNAME}-vlanid${vlanid}-ipv${proto}\""
+cmd="curl --fail -s -H \"X-IXP-Manager-API-Key: ${KEY}\" ${URL}/${handle} >${dest}"
 
 if [[ $DEBUG -eq 1 ]]; then echo $cmd; fi
 eval $cmd
 
-# We want to be bullet proof here so we really want to check the generated file to try
-# and ensure it is valid
+# We want to be safe here so check the generated file to see whether it
+# looks valid
 
 if [[ $? -ne 0 ]]; then
-    echo "ERROR: non-zero return from wget when generating $dest"
+    echo "ERROR: non-zero return from curl when generating $dest"
     exit 2
 fi
 
@@ -80,7 +87,7 @@ if [[ ! -e $dest || ! -s $dest ]]; then
 fi
 
 if [[ $( cat $dest | grep "protocol bgp pb_" | wc -l ) -lt 2 ]]; then
-    echo "ERROR: <2 BGP protocol defintions in config file $dest - something has gone wrong..."
+    echo "ERROR: fewer than 2 BGP protocol definitions in config file $dest - something has gone wrong..."
     exit 4
 fi
 
@@ -94,18 +101,18 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # config file should be okay; back up the current one
-if [[ -e ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf ]]; then
-    cp "${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf" "${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf.old"
+if [[ -e ${cfile} ]]; then
+    cp "${cfile}" "${cfile}.old"
 fi
-mv $dest ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf
+mv $dest $cfile
 
 # are we running or do we need to be started?
-cmd="${BIN}c${PROTOCOL} -s ${RUNPATH}/bird-vlanid${vlanid}-ipv${proto}.ctl show memory"
+cmd="${BIN}c${PROTOCOL} -s $socket show memory"
 if [[ $DEBUG -eq 1 ]]; then echo $cmd; fi
 eval $cmd &>/dev/null
 
 if [[ $? -ne 0 ]]; then
-    cmd="${BIN}${PROTOCOL} -c ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf -s ${RUNPATH}/bird-vlanid${vlanid}-ipv${proto}.ctl"
+    cmd="${BIN}${PROTOCOL} -c ${cfile} -s $socket"
 
     if [[ $DEBUG -eq 1 ]]; then echo $cmd; fi
     eval $cmd &>/dev/null
@@ -115,18 +122,18 @@ if [[ $? -ne 0 ]]; then
         exit 5
     fi
 else
-    cmd="${BIN}c${PROTOCOL} -s ${RUNPATH}/bird-vlanid${vlanid}-ipv${proto}.ctl configure"
+    cmd="${BIN}c${PROTOCOL} -s $socket configure"
     if [[ $DEBUG -eq 1 ]]; then echo $cmd; fi
     eval $cmd &>/dev/null
 
     if [[ $? -ne 0 ]]; then
         echo "ERROR: Reconfigure failed for $dest"
 
-        if [[ -e ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf.old ]]; then
+        if [[ -e ${cfile}.old ]]; then
             echo "Trying to revert to previous"
-            mv ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf $dest
-            mv ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf.old ${ETCPATH}/bird-vlanid${vlanid}-ipv${proto}.conf
-            cmd="${BIN}c${PROTOCOL} -s ${RUNPATH}/bird-vlanid${vlanid}-ipv${proto}.ctl configure"
+            mv ${cfile}.conf $dest
+            mv ${cfile}.old ${cfile}
+            cmd="${BIN}c${PROTOCOL} -s $socket configure"
             if [[ $DEBUG -eq 1 ]]; then echo $cmd; fi
             eval $cmd &>/dev/null
             if [[ $? -eq 0 ]]; then
