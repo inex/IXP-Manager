551f308ea5b44e0a5f031036ab2e60c2c20bbebc
Barry O'Donovan <barry@opensolutions.ie>
2017-01-12 20:51:02 +0000

    [HK] Laravel helper for IDEs - specifically PhpStorm

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,10 @@ class AppServiceProvider extends ServiceProvider {
      */
     public function register()
     {
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
+
         $this->app->bind(
             'Illuminate\Contracts\Auth\Registrar',
             'IXP\Services\Registrar'
