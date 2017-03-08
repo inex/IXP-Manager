a638af4e7fc2d90ac58b5b5871023b1c694608df
Barry O'Donovan <barry@opensolutions.ie>
2017-03-08 07:09:46 +0000

    Really remove requirement for 7.1 - fixes #298
    For now...

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2017-03-01.
+    * Generated on 2017-03-08.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
