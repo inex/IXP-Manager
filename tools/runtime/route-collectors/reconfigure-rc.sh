4e5bbe38c23a4cf6cc2023f25d692d0a74721236
Barry O'Donovan <barry@opensolutions.ie>
2017-05-23 21:06:46 +0100

    Additional scripts for router config generation, fix Travis

--- tools/runtime/route-collectors/reconfigure-rc.sh
+++ tools/runtime/route-collectors/reconfigure-rc.sh
@@ -1,33 +1,8 @@
 #! /usr/bin/env bash
-#
-# Copyright (C) 2009-2017 Internet Neutral Exchange Association Company Limited By Guarantee.
-# All Rights Reserved.
-#
-# This file is part of IXP Manager.
-#
-# IXP Manager is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation, version 2.0 of the License.
-#
-# IXP Manager is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-# more details.
-#
-# You should have received a copy of the GNU General Public License v2.0
-# along with IXP Manager.  If not, see:
-#
-# http://www.gnu.org/licenses/gpl-2.0.html
-#
-# Barry O'Donovan <barry@islandbridgenetworks.ie> 2016
 
-# Example script for updating AS112 Bird BGP configs
-
-
-# For keys, see: http://docs.ixpmanager.org/features/api/
-KEY="my-ixp-manager-api-key"
-URL="https://ixp.example.com/api/v4/router/gen-config"
-URL_DONE="https://ixp.example.com/api/v4/router/updated"
+KEY="your-ixp-manager-api-key"
+URL="http://ixp.example.com/api/v4/router/gen-config"
+URL_DONE="http://ixp.example.com/api/v4/router/updated"
 ETCPATH="/usr/local/etc/bird"
 RUNPATH="/var/run/bird"
 LOGPATH="/var/log/bird"
@@ -41,7 +16,7 @@ if [[ -n $1 && $1 = '--quiet' ]]; then
     export QUIET=1
 else
     export QUIET=0
-    echo -en "\nIXP AS112 BGPd Lisenters\n==============================\n\n"
+    echo -en "\Route Collector BGPd Lisenters\n==============================\n\n"
     echo -e "Verbose mode enabled. Issue --quiet for non-verbose mode (--debug also available)\n"
 fi
 
@@ -58,11 +33,7 @@ function log {
     fi
 }
 
-# These are the handles as configured in your IXP Manager - see: http://docs.ixpmanager.org/features/routers/
-#
-# This script assumes v6 versions end in -ipv6
-
-for handle in my-as112-router1-ipv4 my-as112-router1-ipv6; do
+for handle in handle1-ipv4 handle1-ipv6; do
 
     if [[ $handle == *ipv6 ]]; then
         PROTOCOL="6"
@@ -90,7 +61,7 @@ for handle in my-as112-router1-ipv4 my-as112-router1-ipv6; do
     eval $cmd &>/dev/null
 
     if [[ $? -eq 0 ]]; then
-        log "RECONFIGURED \tIXP Manager Updated:"
+        log "RECONFIGURED \tIXP Manager Updated: "
     else
         cmd="${BIN}${PROTOCOL} -c ${ETCPATH}/bird-${handle}.conf -s ${RUNPATH}/bird-${handle}.ctl"
 
@@ -98,7 +69,7 @@ for handle in my-as112-router1-ipv4 my-as112-router1-ipv6; do
         eval $cmd &>/dev/null
 
         if [[ $? -eq 0 ]]; then
-            log "STARTED \tIXP Manager Updated:"
+            log "STARTED \tIXP Manager Updated: "
         else
             log "ERROR\n"
             continue
