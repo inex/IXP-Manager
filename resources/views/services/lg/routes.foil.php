9ec6275cbe54ba5688fbf9da679f356804139f1f
Barry O'Donovan <barry@opensolutions.ie>
2017-06-08 20:43:08 +0100

    [NF] Add support for Bird's Eye's new large BGP community support

--- resources/views/services/lg/routes.foil.php
+++ resources/views/services/lg/routes.foil.php
@@ -14,6 +14,7 @@
             <th>Next Hop</th>
             <th></th>
             <th>Metric</th>
+            <th>Communities?</th>
             <th>AS Path</th>
             <th></th>
         </tr>
@@ -50,6 +51,21 @@
             <?php endif; ?>
         </td>
         <td><?= $r->metric ?></td>
+        <td>
+            <span class="badge">
+                @if( isset( $r->bgp->communities ) )
+                    {{ count( $r->bgp->communities ) }}
+                @else
+                    0
+                @endif
+            </span>
+
+            @if( isset( $r->bgp->large_communities ) )
+                <span class="badge">LC:
+                    {{ count( $r->bgp->large_communities ) }}
+                </span>
+            @endif
+        </td>
         <td>
             <?php if( isset($r->bgp->as_path) ): ?>
                 <?= implode(' ', $r->bgp->as_path) ?>
