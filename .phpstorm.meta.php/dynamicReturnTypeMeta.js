0bd9accff7b45cec350c9ab39df56119bac171c8
Barry O'Donovan <barry@opensolutions.ie>
2017-08-31 17:53:34 +0100

    [IDE] Oh yeah, finally autocompletion for dynamic return types

---
+++ .phpstorm.meta.php/dynamicReturnTypeMeta.js
@@ -0,0 +1,5 @@
+
+function replaceWithRepository( returnTypeNameSpace, returnTypeClass ){
+    //api.writeToEventLog("Repositories\\" + returnTypeClass)
+    return "Repositories\\" + returnTypeClass;
+}
