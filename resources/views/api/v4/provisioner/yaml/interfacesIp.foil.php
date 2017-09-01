1a938b5d06b5b55627ce8e1d53b40b91037e1769
Nick Hilliard <nick@foobar.org>
2017-09-01 17:51:51 +0100

    [BF] fix white spacing for core-link-interface output

--- resources/views/api/v4/provisioner/yaml/interfacesIp.foil.php
+++ resources/views/api/v4/provisioner/yaml/interfacesIp.foil.php
@@ -1,18 +1,19 @@
 interfacesip:
+<?php foreach( $t->cis as $ci ): ?>
+  -
+    description: "<?= $ci['description'] ?>"
+    name: <?= $ci['name'] ?>
 
-<div>
-    <?php foreach( $t->cis as $ci ): ?>
-        <div>
-            - name: <?= $ci['name'] ?><br/>
-            &nbsp;&nbsp;description: <?= $ci['description'] ?><br/>
-            &nbsp;&nbsp;shutdown: <?= $ci['enabled'] ? 'No' : 'Yes' ?><br/>
-            &nbsp;&nbsp;bfd: <?= $ci['bfd'] ? 'Yes' : 'No' ?><br/>
-            &nbsp;&nbsp;ipv4: <?= isset($ci['ip']) ? $ci['ip'] : '' ?><br/>
-        </div>
-    <?php endforeach; ?>
-    <div>
-        - description: Loopback interface<br/>
-        &nbsp;&nbsp;ipv4: <?= $t->switch->getLoopbackIP() ? $t->switch->getLoopbackIP() : '' ?><br/>
-        &nbsp;&nbsp;loopback: <?= $t->switch->getLoopbackIP() ? 'yes' : 'no' ?><br/>
-        &nbsp;&nbsp;Name: <?= $t->switch->getLoopbackName() ? $t->switch->getLoopbackName() : '' ?><br/>
-</div>
+    ipv4: <?= isset($ci['ip']) ? $ci['ip'] : '' ?>
+
+    shutdown: <?= $ci['enabled'] ? 'no' : 'yes' ?>
+
+    bfd: <?= $ci['bfd'] ? 'yes' : 'no' ?>
+
+<?php endforeach; ?>
+  -
+    description: "Loopback interface"
+    name: <?= $t->switch->getLoopbackName() ? $t->switch->getLoopbackName() : '' ?>
+
+    ipv4: <?= $t->switch->getLoopbackIP() ? $t->switch->getLoopbackIP() : '' ?>/32
+    loopback: yes
