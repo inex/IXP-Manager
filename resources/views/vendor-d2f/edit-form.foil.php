f389a1054aaba7f2c786fcf75b2f786dcc4715db
Yann Robin <yann@islandbridgenetworks.ie>
2017-10-11 15:43:22 +0100

    [NF] - CRUD Vendor - islandbridgenetworks/IXP-Manager#58

---
+++ resources/views/vendor-d2f/edit-form.foil.php
@@ -0,0 +1,43 @@
+<div class="well col-sm-12">
+
+    <?= Former::open()->method( 'POST' )
+        ->id( 'form' )
+        ->action( action( $t->controller.'@store' ) )
+        ->customWidthClass( 'col-sm-3' )
+    ?>
+
+    <?= Former::text( 'name' )
+        ->label( 'Name' )
+        ->blockHelp( "" );
+    ?>
+
+    <?= Former::text( 'shortname' )
+        ->label( 'Shortname' )
+        ->blockHelp( "" );
+    ?>
+
+    <?= Former::text( 'nagios-name' )
+        ->label( 'Nagios Name' )
+        ->blockHelp( "" );
+    ?>
+
+    <?= Former::text( 'bundle-name' )
+        ->label( 'Bundle Name' )
+        ->blockHelp( "" );
+    ?>
+
+    <?= Former::actions(
+        Former::primary_submit( 'Save Changes' ),
+        Former::default_link( 'Cancel' )->href( action ($t->controller.'@list') ),
+        Former::success_button( 'Help' )->id( 'help-btn' )
+    );
+    ?>
+
+    <?= Former::hidden( 'id' )
+        ->value( $t->params[ 'object'] ? $t->params[ 'object']->getId() : '' )
+    ?>
+
+    <?= Former::close() ?>
+
+</div>
+
--- routes/web-doctrine2frontend.php
+++ routes/web-doctrine2frontend.php
@@ -12,4 +12,5 @@ IXP\Http\Controllers\CustKitController::routes();
 IXP\Http\Controllers\InfrastructureController::routes();
 IXP\Http\Controllers\LocationController::routes();
 IXP\Http\Controllers\CabinetController::routes();
+IXP\Http\Controllers\VendorController::routes();
 
