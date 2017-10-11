a59e736c40da86d4d7c5836ff4f27bfba0927fa3
Barry O'Donovan <barry@opensolutions.ie>
2017-10-11 15:52:27 +0100

    [BF] opps, out by 1000x

--- resources/views/interfaces/core-bundle/list.foil.php
+++ resources/views/interfaces/core-bundle/list.foil.php
@@ -85,7 +85,7 @@ $this->layout( 'layouts/ixpv4' );
                             <?= $t->ee( $cb->getSwitchSideX( false )->getName() )  ?>
                         </td>
                         <td>
-                            <?= $t->scaleBits( count( $cb->getCoreLinks() ) * $cb->getSpeedPi() * 1000000000, 0 )  ?>
+                            <?= $t->scaleBits( count( $cb->getCoreLinks() ) * $cb->getSpeedPi() * 1000000, 0 )  ?>
                         </td>
                         <td>
                             <div class="btn-group btn-group-sm" role="group">
