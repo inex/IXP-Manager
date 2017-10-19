52237a654c2e6157de14dd1f2ae54982f6d6f117
Barry O'Donovan <barry@opensolutions.ie>
2017-10-19 14:12:24 +0100

    Documentation for console server connections

---
+++ resources/views/console-server-connection/list-postamble.foil.php
@@ -0,0 +1,12 @@
+
+<br /><br />
+<div class="well">
+    <h3>Console Server Connections</h3>
+
+    <p>
+        Out of band access (for emergencies, firmware upgrades, etc) is normally achieved with the use of a console server of some form.
+        These devices provide console port access to switches. Depending on your console server type, it may not have an easy interface
+        for recording what port is connected to what device. You can use this page to record those details yourself.
+    </p>
+
+</div>
