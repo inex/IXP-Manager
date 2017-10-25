06d4a6f9e3b94283e718c0feda23c5f69ca1feec
Yann Robin <yann@islandbridgenetworks.ie>
2017-10-25 12:51:02 +0100

    [BF] - corelinks.enabled does not appear in provisioner API export -  fixes islandbridgenetworks/IXP-Manager#67

--- database/Repositories/Switcher.php
+++ database/Repositories/Switcher.php
@@ -521,7 +521,7 @@ class Switcher extends EntityRepository
 
         foreach( [ 'A', 'B' ] as $side ) {
             /** @noinspection SqlNoDataSourceInspection */
-            $dql = "SELECT cb.type, cb.ipv4_subnet as cbSubnet, cl.enabled, cb.description, cl.bfd, sp$side.name, pi$side.speed, cl.ipv4_subnet as clSubnet, s$side.id as saId
+            $dql = "SELECT cb.type, cb.ipv4_subnet as cbSubnet,cb.enabled as cbEnabled, cl.enabled as clEnabled, cb.description, cl.bfd, sp$side.name, pi$side.speed, cl.ipv4_subnet as clSubnet, s$side.id as saId
                         FROM Entities\\CoreLink cl
                         LEFT JOIN cl.coreBundle cb
 
@@ -552,7 +552,7 @@ class Switcher extends EntityRepository
                 $export[ 'bfd' ]          = $ci[ 'bfd' ];
                 $export[ 'speed' ]        = $ci[ 'speed' ];
                 $export[ 'name' ]         = $ci[ 'name' ];
-                $export[ 'shutdown' ]     = !$ci[ 'enabled' ];
+                $export[ 'shutdown' ]     = $ci[ 'cbEnabled' ] && $ci[ 'clEnabled' ] ? false : true;
 
                 $cis[] = $export;
             }
