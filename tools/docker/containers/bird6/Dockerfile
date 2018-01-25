b1245f7c8b74d22054f101f400c4353101e1bc7d
Barry O'Donovan <barry@opensolutions.ie>
2018-01-25 13:18:38 +0000

    Start of Docker for development

---
+++ tools/docker/containers/bird6/Dockerfile
@@ -0,0 +1,27 @@
+FROM ubuntu:16.04
+
+MAINTAINER Barry O'Donovan <barry.odonovan@inex.ie>
+
+EXPOSE 179
+
+RUN apt-get -y update && \
+    apt-get -y upgrade && \
+    apt-get -y autoremove --purge && \
+    apt-get -y clean && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm -rf /tmp/*
+
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 52463488670E69A092007C24F2331238F9C59A45 && \
+    echo "\ndeb http://ppa.launchpad.net/cz.nic-labs/bird/ubuntu xenial main" >>/etc/apt/sources.list && \
+    apt-get -y update && \
+    apt-get -y install bird && \
+    apt-get -y autoremove --purge && \
+    apt-get -y clean && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm -rf /tmp/* && \
+    mkdir -p /run/bird /var/log/bird && \
+    chown bird: /run/bird /var/log/bird
+
+WORKDIR /
+
+ENTRYPOINT [ "/usr/sbin/bird6", "-f", "-u", "bird", "-g", "bird" ]
---
+++ tools/docker/containers/rs1/api-reconfigure-all-v4.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Copyright (C) 2009-2016 Internet Neutral Exchange Association Company Limited By Guarantee.
+# All Rights Reserved.
+#
+# This file is part of IXP Manager.
+#
+# IXP Manager is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2.0 of the License.
+#
+# IXP Manager is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License v2.0
+# along with IXP Manager.  If not, see:
+#
+# http://www.gnu.org/licenses/gpl-2.0.html
+#
+# Example script used by INEX to rebuild all Bird route servers via API on demand.
+#
+# Author: Barry O'Donovan <barry@opensolutions.ie>
+
+while true; do
+    /usr/bin/wget -O /dev/null -q http://ixpmanager-www/api/v4/test;
+    if [[ $? -eq 0 ]]; then
+        break;
+    fi;
+    echo 'Waiting for ixpmanager/www';
+    sleep 2;
+done
+
+echo "Reconfiguring all bird instances:"
+
+# These handles should match the definitions in config/routers.php, and
+# should be changed as appropriate:
+
+for handle in rs1-ipv4 rs1-ipv6; do
+    echo -ne "HANDLE: ${handle}: "
+    /usr/local/sbin/api-reconfigure-v4.sh -h $handle -q
+    if [[ $? -eq 0 ]]; then
+        echo -ne "OK    "
+    else
+        echo -ne "ERROR "
+    fi
+    echo
+done
