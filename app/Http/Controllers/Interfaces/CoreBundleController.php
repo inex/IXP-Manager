f8c700f7a6e381561add120b2b9c43664c64bc25
Yann Robin <yann@islandbridgenetworks.ie>
2018-02-06 14:49:33 +0000

    [BF] - deleting a core bundle resets the switch port type to be Unset / Unknown  - fixes islandbridgenetworks/IXP-Manager#115

--- app/Http/Controllers/Interfaces/CoreBundleController.php
+++ app/Http/Controllers/Interfaces/CoreBundleController.php
@@ -401,10 +401,6 @@ class CoreBundleController extends Common
 
                 $vi = $pi->getVirtualInterface();
 
-                /** @var SwitchPortEntity $sp */
-                $sp = $vi->getSwitchPort();
-                $sp->setType( SwitchPortEntity::TYPE_CORE );
-
                 D2EM::remove( $ci );
                 D2EM::remove( $pi );
                 D2EM::remove( $vi );
