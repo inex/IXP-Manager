a4b4206e045c949c683dc18967973556c2299f1c
Barry O'Donovan <barry@opensolutions.ie>
2018-06-18 09:51:06 +0100

    [CI WIP] demo and instructions for @yannrobin

--- tests/Browser/RouterControllerTest.php
+++ tests/Browser/RouterControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Browser;
 
 use D2EM;
 
+use Entities\Router as RouterEntity;
 use Entities\User as UserEntity;
 
 use Tests\DuskTestCase;
@@ -31,6 +32,7 @@ class RouterControllerTest extends DuskTestCase
             $browser->visit( '/router/add' )
                     ->assertSee( 'Handle' );
 
+            // 1. test add
             $browser->type('handle',  'dusk-ci-test')
                 ->select( 'vlan',     '2' )
                 ->select( 'protocol', '6' )
@@ -51,6 +53,29 @@ class RouterControllerTest extends DuskTestCase
                 ->type( 'template', 'api/v4/router/server/bird/standard' )
                 ->press('Add Router')
                 ->assertPathIs('/router/list');
+
+            /** @var RouterEntity $router */
+            $router = D2EM::getRepository( RouterEntity::class )->findOneBy( [ 'handle' => 'dusk-ci-test' ] );
+
+            // 2. test added data in database against expected values
+            $this->assertInstanceOf( RouterEntity::class, $router );
+            $this->assertEquals( 'Travis CI Test', $router->getName() );
+            // repeat for all elements
+
+            // 3. browse to edit router object: $browser->visit( '/router/edit/' . $router->getId() )
+            // 4. test that form contains settings as above using assertChecked(), assertNotChecked(), assertSelected(), assertInputValue, ...
+
+            // 5. uncheck checkboxes, change selects and values, ->press('Save Changes'), assertPathIs(....)  (repeat 1)
+            // 6. repeat database load and database object check for new values (repeat 2)
+
+            // 7. edit again and assert that all checkboxes are unchecked and assert select values are as expected
+            // 8. submit with no changes and verify no changes in database
+
+            // 9. edit again and check all checkboxes and submit
+            // 10. verify checkbox bool elements in database are all true
+
+            // 11. delete the router in the UI and verify via success message text and location
+            // 12. do a D2EM findOneBy and verify false/null 
         });
     }
 }
