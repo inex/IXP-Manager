80d3f3af44326eeb703c01ad80196ad0a9563288
Barry O'Donovan <barry@opensolutions.ie>
2019-01-07 14:28:38 +0000

    Switch to a versioned version of OSS_SNMP [skip-ci]

--- .phpstorm.meta.php/laravel.meta.php
+++ .phpstorm.meta.php/laravel.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-01-07 11:05:07.
+    * Generated on 2019-01-07 14:28:03.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.44 on 2019-01-07 11:05:06.
+ * Generated for Laravel 5.5.44 on 2019-01-07 14:28:02.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
