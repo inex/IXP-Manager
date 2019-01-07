bf80b1f9b761009b733d3be2e7290c64bb355b0f
Barry O'Donovan <barry@opensolutions.ie>
2019-01-07 16:52:46 +0000

    Update OSS_SNMP version - should fix TravisCI also

--- .phpstorm.meta.php/laravel.meta.php
+++ .phpstorm.meta.php/laravel.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-01-07 14:28:03.
+    * Generated on 2019-01-07 16:51:23.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.44 on 2019-01-07 14:28:02.
+ * Generated for Laravel 5.5.44 on 2019-01-07 16:51:22.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
