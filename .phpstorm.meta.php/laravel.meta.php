9b6d4b25a1f9e0b07feb9a8fde75c5f97511973b
Barry O'Donovan <barry@opensolutions.ie>
2019-01-21 09:29:28 +0000

    Package updates

--- .phpstorm.meta.php/laravel.meta.php
+++ .phpstorm.meta.php/laravel.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-01-07 16:51:23.
+    * Generated on 2019-01-21 09:27:53.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
