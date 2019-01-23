05acc047b3aae95c9d9a43030bc20954ce52e249
Barry O'Donovan <barry@opensolutions.ie>
2019-01-23 07:17:07 +0000

    Add documentation links to router management

--- resources/views/router/edit.foil.php
+++ resources/views/router/edit.foil.php
@@ -6,6 +6,9 @@
 
 <?php $this->section( 'page-header-postamble' ) ?>
     <div class="btn-group btn-group-sm" role="group">
+        <a target="_blank" class="btn btn-outline-secondary" href="https://docs.ixpmanager.org/features/routers/">
+            Documentation
+        </a>
         <a class="btn btn-outline-secondary" href="<?= route('router@list' ) ?>" title="list">
             <i class="fa fa-th-list"></i>
         </a>
