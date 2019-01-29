e564ec2f6efd359da146c74af765a27afaad8cd3
Barry O'Donovan <barry@opensolutions.ie>
2019-01-29 12:16:41 +0000

    Add Bird v2 templates for AS112

---
+++ resources/views/api/v4/router/as112/bird2/neighbors.foil.php
@@ -0,0 +1,25 @@
+
+<?php foreach( $t->ints as $int ):
+    
+        // do not set up a session to ourselves!
+        if( $int['autsys'] == $t->router->asn() ):
+            continue;
+        endif;
+
+?>
+
+protocol bgp pb_as<?= $int['autsys'] ?>_vli<?= $int['vliid'] ?>_ipv<?= $int['protocol'] ?? 4 ?> {
+        description "AS<?= $int['autsys'] ?> - <?= $int['cname'] ?>";
+        local as routerasn;
+        source address routeraddress;
+        neighbor <?= $int['address'] ?> as <?= $int['autsys'] ?>;
+        ipv<?= $int['protocol'] ?? 4 ?> {
+            import filter f_import_policy;
+            export where proto = "static_as112";
+            import limit <?= $int['maxprefixes'] ?> action restart;
+        };
+        <?php if( $int['bgpmd5secret'] && !$t->router->skipMD5() ): ?>password "<?= $int['bgpmd5secret'] ?>";<?php endif; ?>
+
+}
+
+<?php endforeach; ?>
