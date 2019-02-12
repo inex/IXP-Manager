40510e8bd5c367d49c782d57f2735ff1b317c257
Barry O'Donovan <barry@opensolutions.ie>
2019-02-12 09:56:33 +0000

    Do not filter transit peers when filtering transit asns in the path

--- resources/views/api/v4/router/collector/bird2/filter-transit-networks.foil.php
+++ resources/views/api/v4/router/collector/bird2/filter-transit-networks.foil.php
@@ -34,7 +34,7 @@ function filter_has_transit_path()
 int set transit_asns;
 {
     transit_asns = TRANSIT_ASNS;
-    if (bgp_path ~ transit_asns) then {
+    if (bgp_path.first !~ transit_asns && bgp_path ~ transit_asns) then {
         bgp_large_community.add( IXP_LC_FILTERED_TRANSIT_FREE_ASN );
         return true;
     }
