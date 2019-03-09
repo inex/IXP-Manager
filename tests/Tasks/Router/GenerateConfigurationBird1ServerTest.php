f5e0f72b3e7042ff46961b7f7dd3384b79b15da6
Barry O'Donovan <barry@opensolutions.ie>
2019-03-09 15:37:09 +0000

    Refector Bird1 config tests

--- tests/Tasks/Router/GenerateConfigurationBird1ServerTest.php
+++ tests/Tasks/Router/GenerateConfigurationBird1ServerTest.php
@@ -36,29 +36,9 @@ use Tests\TestCase;
  * @copyright  Copyright (C) 2009 - 2019 Internet Neutral Exchange Association Company Limited By Guarantee
  * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU GPL V2.0
  */
-class GenerateConfigurationTest extends TestCase
+class GenerateConfigurationBird1ServerTest extends TestCase
 {
-    public $rchandles    = [ 'rc1-lan1-ipv4',   'rc1-lan1-ipv6',   'rc1-lan2-ipv4',   'rc1-lan2-ipv6',    ];
     public $rshandles    = [ 'rs1-lan1-ipv4',   'rs1-lan1-ipv6',   'rs1-lan2-ipv4',   'rs1-lan2-ipv6',    ];
-    public $as112handles = [ 'as112-lan1-ipv4', 'as112-lan1-ipv6', 'as112-lan2-ipv4', 'as112-lan2-ipv6',  ];
-
-    public function testRouteCollectorBirdConfigurationGeneration()
-    {
-        foreach( $this->rchandles as $handle )
-        {
-            $router = D2EM::getRepository( RouterEntity::class )->findOneBy( ['handle' => $handle] );
-            $conf = ( new RouterConfigurationGenerator( $router ) )->render();
-
-            $knownGoodConf = file_get_contents( base_path() . "/data/travis-ci/known-good/ci-apiv4-{$handle}.conf" );
-            $this->assertFalse( $knownGoodConf === false, "RC Conf generation - could not load known good file ci-apiv4-{$handle}.conf" );
-
-            // clean the configs to remove the comment lines which are irrelevent
-            $conf          = preg_replace( "/^#.*$/m", "", $conf          );
-            $knownGoodConf = preg_replace( "/^#.*$/m", "", $knownGoodConf );
-
-            $this->assertEquals( $knownGoodConf, $conf, "Known good and generated RC configuration for {$handle} do not match" );
-        }
-    }
 
     public function testRouteServerBirdConfigurationGeneration()
     {
@@ -75,24 +55,6 @@ class GenerateConfigurationTest extends TestCase
             $knownGoodConf = preg_replace( "/^#.*$/m", "", $knownGoodConf );
 
             $this->assertEquals( $knownGoodConf, $conf, "Known good and generated RS configuration for {$handle} do not match" );
-        }
-    }
-
-    public function testAs112BirdConfigurationGeneration()
-    {
-        foreach( $this->as112handles as $handle )
-        {
-            $router = D2EM::getRepository( RouterEntity::class )->findOneBy( ['handle' => $handle] );
-            $conf = ( new RouterConfigurationGenerator( $router ) )->render();
-
-            $knownGoodConf = file_get_contents( base_path() . "/data/travis-ci/known-good/ci-apiv4-{$handle}.conf" );
-            $this->assertFalse( $knownGoodConf === false, "AS112 Conf generation - could not load known good file ci-apiv4-{$handle}.conf" );
-
-            // clean the configs to remove the comment lines which are irrelevent
-            $conf          = preg_replace( "/^#.*$/m", "", $conf          );
-            $knownGoodConf = preg_replace( "/^#.*$/m", "", $knownGoodConf );
-
-            $this->assertEquals( $knownGoodConf, $conf, "Known good and generated AS112 configuration for {$handle} do not match" );
         }
     }
 }
