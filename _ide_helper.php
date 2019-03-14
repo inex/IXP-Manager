2fa3798127543750945408d7527f32f3b4648070
Barry O'Donovan <barry@opensolutions.ie>
2019-03-14 09:37:24 +0000

    s/my temp laravel-doctrine/orm for laravel 5.8/official version

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.4 on 2019-03-14 09:03:58.
+ * Generated for Laravel 5.8.4 on 2019-03-14 09:36:12.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
