5dba1f3b82967a75910126d27c7e157a3a807eb7
Barry O'Donovan <barry@opensolutions.ie>
2019-03-14 09:08:42 +0000

    Vendor updates and commiting JS and CSS to repo

--- .phpstorm.meta.php/laravel.meta.php
+++ .phpstorm.meta.php/laravel.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-03-13 12:08:15.
+    * Generated on 2019-03-14 09:03:58.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
