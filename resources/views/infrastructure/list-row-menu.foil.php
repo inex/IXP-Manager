ef6a75599ad07c9dcb32ea06237df166d7386f37
Yann Robin <yann@islandbridgenetworks.ie>
2019-04-24 15:20:38 +0100

    [DUSK] - Infrastructure + facility

--- resources/views/infrastructure/list-row-menu.foil.php
+++ resources/views/infrastructure/list-row-menu.foil.php
@@ -5,7 +5,7 @@
     </a>
 
     <?php if( !isset( $t->feParams->readonly ) || !$t->feParams->readonly ): ?>
-        <a class="btn btn-outline-secondary" href="<?= route($t->feParams->route_prefix . '@edit' , [ 'id' => $t->row[ 'id' ] ] ) ?> " title="Edit">
+        <a id="d2f-list-edit-<?= $t->row[ 'id' ] ?>" class="btn btn-outline-secondary" href="<?= route($t->feParams->route_prefix . '@edit' , [ 'id' => $t->row[ 'id' ] ] ) ?> " title="Edit">
             <i class="fa fa-pencil"></i>
         </a>
         <a class="btn btn-outline-secondary d2f-list-delete" id='d2f-list-delete-<?= $t->row[ 'id' ] ?>' href="#" data-object-id="<?= $t->row[ 'id' ] ?>" title="Delete">
