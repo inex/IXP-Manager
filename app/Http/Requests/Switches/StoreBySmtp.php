6b6f08562e41850c98f25e9ee41cfc04e4383212
Yann Robin <yann@islandbridgenetworks.ie>
2019-07-19 15:00:55 +0100

    Domain name checking - fixes #216 and fixes inex/IXP-Manager#499
    * [BF] - domain name checking - fixes islandbridgenetworks/IXP-Manager#216
    
    * [BF] - domain name checking - fixes islandbridgenetworks/IXP-Manager#216
    
    * [BF] - Domain name checking as custom validation rule
    
    * [IM] - refactor custom store - islandbridgenetworks/IXP-Manager#216

---
+++ app/Http/Requests/Switches/StoreBySmtp.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace IXP\Http\Requests\Switches;
+
+/*
+ * Copyright (C) 2009 - 2019 Internet Neutral Exchange Association Company Limited By Guarantee.
+ * All Rights Reserved.
+ *
+ * This file is part of IXP Manager.
+ *
+ * IXP Manager is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, version v2.0 of the License.
+ *
+ * IXP Manager is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License v2.0
+ * along with IXP Manager.  If not, see:
+ *
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+use Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use IXP\Rules\IdnValidate;
+
+class StoreBySmtp extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // middleware ensures superuser access only so always authorised here:
+        return Auth::getUser()->isSuperUser();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'snmppasswd' => 'required|string|max:255',
+            'hostname'   => [ 'required', 'string', 'max:255', 'unique:Entities\Switcher,hostname' . ( $this->input('id') ? ','. $this->input('id') : '' ), new IdnValidate() ],
+
+        ];
+    }
+
+}
