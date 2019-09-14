1c8a854e7eb8c3aeadbd2ddc32efc947eacba9ae
Barry O'Donovan <barry@opensolutions.ie>
2019-09-14 12:04:54 +0100

    Update packages

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.0.3 on 2019-09-14 09:25:25.
+ * Generated for Laravel 6.0.3 on 2019-09-14 09:32:26.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
