e20e28c5f652bd87b3673b7cddf65d9573cb1192
Yann Robin <yann@islandbridgenetworks.ie>
2019-10-07 10:26:15 +0100

    [IM] - Add routers tabs Looking Glass

--- resources/views/services/lg/index.foil.php
+++ resources/views/services/lg/index.foil.php
@@ -6,18 +6,42 @@
 
 <?php $this->section('content') ?>
 
+<?php if( count( $t->tabRouters ) == 1 ): ?>
 
-<div class="alert alert-info" role="alert">
-    <div class="d-flex align-items-center">
-        <div class="text-center">
-            <i class="fa fa-question-circle fa-2x"></i>
+    <?= $t->insert('services/lg/router-tab' ) ?>
+
+<?php else: ?>
+    <div class="card mt-4">
+        <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs" id="infra-tab">
+                <?php foreach ( $t->tabRouters as $infra => $router): ?>
+                    <li role="<?= Str::kebab( strtolower( $infra ) ) ?>" class="nav-item">
+                        <a class="nav-link <?= !($infra === array_key_first( $t->tabRouters ) ) ?: 'active'?>" data-toggle="tab" href="#<?= Str::kebab( strtolower( $infra ) ) ?>">
+                            <?= $infra ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
-        <div class="col-sm-12">
-            Please select a router to examine from the dropdown on the top right.
+
+        <div class="card-body">
+            <?= $t->insert('services/lg/router-tab' ) ?>
         </div>
     </div>
-</div>
+<?php endif; ?>
 
 
+<?php $this->append() ?>
 
+<?php $this->section( 'scripts' ) ?>
+<script>
+    $(document).ready(function(){
+
+        $('.nav-link').hover( function (e) {
+            e.preventDefault();
+            $(this).tab('show')
+        })
+
+    });
+</script>
 <?php $this->append() ?>
---
+++ resources/views/services/lg/router-tab.foil.php
@@ -0,0 +1,58 @@
+<div class="tab-content">
+    <?php foreach ( $t->tabRouters as $infra => $info ): ?>
+        <?php $formatedName = Str::kebab( strtolower( $infra ) ) ?>
+        <div id="<?= $formatedName ?>" class="tab-pane fade <?= !($infra === array_key_first( $t->tabRouters ) ) ?: 'active show'?>">
+            <div class="card">
+                <div class="card-header">
+                    <ul class="nav nav-pills card-header-pills">
+
+                        <?php foreach( $info as $protocol => $routers ): ?>
+                            <li class="nav-item" id="peering-potential-li" role="<?= $formatedName . '-' . $protocol ?>" >
+                                <a class="nav-link <?= !($protocol === array_key_first( $info ) ) ?: 'active'?>" data-toggle="tab" href="#<?= $formatedName . '-' . $protocol ?>">IPv<?= $protocol ?></a>
+                            </li>
+                        <?php endforeach; ?>
+
+                    </ul>
+                </div>
+
+                <div class="card-body">
+                    <div class="tab-content">
+                        <?php foreach( $info as $protocol => $routers ): ?>
+                            <div id="<?= $formatedName . '-' . $protocol ?>" class="tab-pane fade <?= !($protocol === array_key_first( $info ) ) ?: 'active show'?>">
+
+                                <table class="table table-striped">
+                                    <thead class="table-dark">
+                                    <th>
+                                        Router
+                                    </th>
+                                    <th>
+                                        Last Updated
+                                    </th>
+                                    <th></th>
+                                    </thead>
+                                    <tbody>
+                                    <?php foreach ( $routers as $router ): ?>
+                                        <tr>
+                                            <td>
+                                                <?= $t->ee( $router[ 'name' ] ) ?>
+                                            </td>
+                                            <td>
+                                                <?= $router[ 'last-updated' ]->format( "Y-m-d H:i:s" ) ?>
+                                            </td>
+                                            <td>
+                                                <a class="btn btn-primary" href="<?= url('/lg/' . $router[ 'handle' ]) ?>">Looking Glass</a>
+                                            </td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        <?php endforeach; ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
