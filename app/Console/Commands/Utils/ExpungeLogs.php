b477eaeeba55bfc53e1aaa71748df9b2a1ee4498
Barry O'Donovan <barry@opensolutions.ie>
2020-01-28 12:35:23 +0000

    [IM] s/midnight/now

--- app/Console/Commands/Utils/ExpungeLogs.php
+++ app/Console/Commands/Utils/ExpungeLogs.php
@@ -87,7 +87,7 @@ class ExpungeLogs extends IXPCommand
         // Deleting expired UserRememberTokens
         $this->isVerbosityVerbose() && $this->output->write('Expunging expired user remember tokens...', false );
         D2EM::createQuery( 'DELETE FROM Entities\\UserRememberToken urt WHERE urt.expires < ?1' )->execute( [
-            1 => now()->format( 'Y-m-d 00:00:00' )
+            1 => now()->format( 'Y-m-d H:i:s' )
         ]);
         $this->isVerbosityVerbose() && $this->info(' [done]' );
 
