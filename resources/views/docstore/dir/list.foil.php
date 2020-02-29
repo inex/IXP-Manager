d6e6f62d750ebaf7849d71ad35aad95de9b8c2d5
Barry O'Donovan <barry@islandbridgenetworks.ie>
2020-02-29 11:26:50 +0000

    Reformat docstore from divs to table

--- resources/views/docstore/dir/list.foil.php
+++ resources/views/docstore/dir/list.foil.php
@@ -17,6 +17,12 @@
                 Documentation
             </a>
 
+            <a id="add-dir" class="btn btn-white" href="<?= route('docstore-dir@list' ) ?>"
+               data-toggle="tooltip" data-placement="bottom" title="Root Directory"
+            >
+                <i class="fa fa-home"></i>
+            </a>
+
             <a id="add-dir" class="btn btn-white" href="<?= route('docstore-dir@create', ['parent_dir' => $t->dir ? $t->dir->id : null ] ) ?>"
                data-toggle="tooltip" data-placement="bottom" title="Create Directory"
             >
@@ -44,100 +50,152 @@
     </div>
 <?php endif; ?>
 
-    <div class="row">
-        <div class="col-md-12">
+    <tr class="row">
+        <tr class="col-md-12">
 
             <?= $t->alerts() ?>
 
-            <?php if( $t->dir ): ?>
-
-                <div class="row tw-py-4 tw-my-0 tw-mx-4 tw-border-b">
-                    <?php if( $t->dir ): ?>
-                        <a class="tw-pr-4 tw-text-black" href="<?= route('docstore-dir@list', ['dir' => $t->dir->parentDirectory ? $t->dir->parentDirectory->id : null] ) ?>"><i class="fa fa-caret-square-o-left fa-2x"></i></a>
-                    <?php endif; ?>
-                    <i class="fa fa-folder-open fa-2x"></i> &nbsp;&nbsp;&nbsp;&nbsp; <?= $t->ee( $t->dir->name ) ?>
-                </div>
-
-            <?php endif; ?>
-
-            <?php foreach( $t->dirs as $i => $dir ): ?>
-
-                <div class="row tw-py-4 tw-my-0 tw-mx-4 tw-border-b <?= $i === 0 && count( $t->dirs ) ? 'tw-border-t' : '' ?>">
-                    <p class="tw-align-middle tw-my-0 tw-mx-4 tw-p-0 mr-auto">
-                        <i class="fa fa-lg fa-folder tw-inline tw-mr-4"></i>
-                         <a href="<?= route('docstore-dir@list', ['dir' => $dir['id']] ) ?>"><?= $t->ee( $dir['name'] ) ?></a>
-                    </p>
-
-                    <?php if( Auth::check() && Auth::user()->isSuperUser() ): ?>
-                        <div class="dropdown">
-                            <button class="btn btn-light btn-sm tw-my-0 tw-py-0" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                &middot;&middot;&middot;
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="<?= route( "docstore-dir@edit", [ "dir" => $dir['id'] ] ) ?>">Edit</a>
-                                <a class="dropdown-item list-delete-btn" data-object-type="dir" href="#" data-url="<?= route('docstore-dir@delete', [ 'dir' => $dir['id'] ] ) ?>">Delete</a>
-                            </div>
-                        </div>
-                    <?php endif; ?>
-                </div>
-
-            <?php endforeach; ?>
 
 
 
-            <?php foreach( $t->files as $i => $file ): ?>
+<div class="docstore">
+<table class="tw-mt-8">
+    <tbody>
 
-                <div class="row tw-py-4 tw-my-0 tw-mx-4 tw-border-b  <?= $i === 0 && !count( $t->dirs ) ? 'tw-border-t' : '' ?>">
-                    <p class="tw-align-middle tw-my-0 tw-mx-4 tw-p-0 mr-auto">
-                        <i class="fa fa-lg fa-file tw-inline tw-mr-4"></i>
-                        <a href="<?= route($file->isViewable() ? 'docstore-file@view' : 'docstore-file@download', ['file' => $file->id] ) ?>"
-                            <?php if( trim( $file->description ) ): ?>
-                                data-toggle="tooltip" data-placement="top" data-html="true" title="<?= parsedown( $file->description ) ?>"
-                            <?php endif; ?>><?= $t->ee( $file->name ) ?></a>
-                    </p>
+    <?php if( $t->dir ): ?>
 
-                    <?php if( Auth::check() && Auth::user()->isSuperUser() ): ?>
-                        <p class="tw-my-0 tw-ml-4 tw-border-gray-200 tw-border tw-rounded tw-bg-gray-200 tw-px-1 tw-text-xs tw-text-gray-700">
-                            <?= \Entities\User::$PRIVILEGES_ALL[ $file->min_privs ] ?>
-                        </p>
+        <tr>
+            <td class="top icon">
+                <?php if( $t->dir ): ?>
+                    <a class="tw-text-black" href="<?= route('docstore-dir@list', ['dir' => $t->dir->parentDirectory ? $t->dir->parentDirectory->id : null] ) ?>">
+                        <i class="fa fa-lg fa-caret-square-o-left tw-inline-block tw-w-full"></i>
+                    </a>
+                <?php endif; ?>
+            </td>
+            <td class="top icon">
+                <i class="fa fa-lg fa-folder-open tw-inline-block tw-w-full"></i>
+            </td>
+            <td class="top tw-px-4 tw-w-auto">
+                <?= $t->ee( $t->dir->name ) ?>
+            </td>
+            <td class="top meta">&nbsp;</td>
+            <td class="top meta">&nbsp;</td>
+            <td class="top meta">&nbsp;</td>
+            <td class="top meta">&nbsp;</td>
+        </tr>
 
-                        <p class="tw-align-middle tw-my-0 tw-mx-4 tw-border-gray-200 tw-border tw-rounded tw-bg-gray-200 tw-px-1 tw-text-sm tw-text-gray-700"
-                                data-toggle="tooltip" data-placement="left" data-html="true" title="User Downloads:<br> <?= $file->downloads_count ?> Total (<?= $file->unique_downloads_count ?> Unique)">
-                            <?= $file->downloads_count ?> (<?= $file->unique_downloads_count ?>)
-                        </p>
-                    <?php endif; ?>
+    <?php endif; ?>
 
-                    <p class="tw-mr-6 tw-ml-4 tw-my-0 tw-text-gray-700 tw-text-sm tw-align-middle tw-border-gray-200 tw-border tw-rounded tw-bg-gray-200 tw-px-1">
-                        <?= $file->file_last_updated->toFormattedDateString() ?>
-                    </p>
+    <?php $i = 0;
+            foreach( $t->dirs as $i => $dir ): ?>
 
+        <tr>
+            <td class="<?= $i ? '' : 'top' ?> icon"></td>
+            <td class="<?= $i ? '' : 'top' ?> icon">
+                <i class="fa fa-lg fa-folder tw-inline-block tw-w-full"></i>
+            </td>
+            <td class="<?= $i ? '' : 'top' ?> tw-px-4 tw-py-2 tw-w-auto">
+                 <a href="<?= route('docstore-dir@list', ['dir' => $dir['id']] ) ?>"><?= $t->ee( $dir['name'] ) ?></a>
+            </td>
+            <td class="<?= $i ? '' : 'top' ?> meta"></td>
+            <td class="<?= $i ? '' : 'top' ?> meta"></td>
+            <td class="<?= $i ? '' : 'top' ?> meta"></td>
+            <td class="<?= $i ? '' : 'top' ?> meta">
+                <?php if( Auth::check() && Auth::user()->isSuperUser() ): ?>
                     <div class="dropdown">
                         <button class="btn btn-light btn-sm tw-my-0 tw-py-0" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             &middot;&middot;&middot;
                         </button>
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                            <?php if( $file->isViewable() ): ?>
-                                <a class="dropdown-item" href="<?= route( 'docstore-file@download', ['file' => $file->id] ) ?>">Download</a>
-                            <?php endif; ?>
-                            <?php if( Auth::check() && Auth::user()->isSuperUser() ): ?>
-                                <a class="dropdown-item list-info-btn" data-object-type="file" href="#" data-url="<?= route( "docstore-file@info", [ "file" => $file ] ) ?>">Metadata</a>
-                            <?php endif; ?>
-                            <a class="dropdown-item" href="#"
-                               onclick="bootbox.alert({ message: 'SHA checksums can be used to check the authenticity / integrity of files.<br><br><?= $file->sha256 ? "SHA256 checksum: [<code>" . $t->ee( $file->sha256 ) . "</code>]" : "there is no sha256 checksum registered for this file." ?>', size: 'large' }); return false;">Show SHA256</a>
-
-                            <?php if( Auth::check() && Auth::user()->isSuperUser() ): ?>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="<?= route( 'docstore-log@unique-list', [ 'file' => $file ] ) ?>">Unique Downloads</a>
-                                <a class="dropdown-item" href="<?= route( 'docstore-log@list', [ 'file' => $file ] ) ?>">All Downloads</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="<?= route( "docstore-file@edit", [ "file" => $file ] ) ?>">Edit</a>
-                                <a class="dropdown-item list-delete-btn" data-object-type="file" href="#" data-url="<?= route( "docstore-file@delete", [ "file" => $file ] ) ?>">Delete</a>
-                            <?php endif; ?>
+                            <a class="dropdown-item" href="<?= route( "docstore-dir@edit", [ "dir" => $dir['id'] ] ) ?>">Edit</a>
+                            <a class="dropdown-item list-delete-btn" data-object-type="dir" href="#" data-url="<?= route('docstore-dir@delete', [ 'dir' => $dir['id'] ] ) ?>">Delete</a>
                         </div>
                     </div>
-                </div>
+                <?php endif; ?>
+            </td>
+        </tr>
 
-            <?php endforeach; ?>
+    <?php $i++; endforeach; ?>
+
+
+
+    <?php foreach( $t->files as $file ): ?>
+
+        <tr class="">
+            <td class="<?= $i ? '' : 'tw-border-t-2' ?> icon"></td>
+            <td class="<?= $i ? '' : 'tw-border-t-2' ?> icon">
+                <i class="fa fa-lg fa-file-o tw-inline-block tw-w-full"></i>
+            </td>
+            <td class="<?= $i ? '' : 'tw-border-t-2' ?> tw-px-4 tw-w-auto">
+                <a href="<?= route($file->isViewable() ? 'docstore-file@view' : 'docstore-file@download', ['file' => $file->id] ) ?>"
+                    <?php if( trim( $file->description ) ): ?>
+                        data-toggle="tooltip" data-placement="top" data-html="true" title="<?= parsedown( $file->description ) ?>"
+                    <?php endif; ?>><?= $t->ee( $file->name ) ?></a>
+            </td>
+
+            <td class="<?= $i ? '' : 'tw-border-t-2' ?> meta tw-text-center">
+                <?php if( Auth::check() && Auth::user()->isSuperUser() ): ?>
+                    <span class="tw-w-full tw-inline-block tw-border-gray-200 tw-border tw-rounded tw-bg-gray-200 tw-px-1 tw-text-xs tw-text-gray-700">
+                        <?= \Entities\User::$PRIVILEGES_ALL[ $file->min_privs ] ?>
+                    </span>
+                <?php endif; ?>
+            </td>
+
+            <td class="<?= $i ? '' : 'tw-border-t-2' ?> meta tw-text-right" style="font-variant-numeric: tabular-nums;">
+                <?php if( Auth::check() && Auth::user()->isSuperUser() ): ?>
+                    <span class="tw-align-middle tw-border-gray-200 tw-border tw-rounded tw-bg-gray-200 tw-px-1 tw-text-sm tw-text-gray-700"
+                            data-toggle="tooltip" data-placement="left" data-html="true" title="User Downloads:<br> <?= $file->downloads_count ?> Total (<?= $file->unique_downloads_count ?> Unique)">
+                        <?= $file->downloads_count ?> (<?= $file->unique_downloads_count ?>)
+                    </span>
+                <?php endif; ?>
+            </td>
+
+            <td class="<?= $i ? '' : 'tw-border-t-2' ?> meta">
+                <span class="tw-text-gray-700 tw-text-sm tw-align-middle tw-border-gray-200 tw-border tw-rounded tw-bg-gray-200 tw-px-1">
+                    <?= $file->file_last_updated->toFormattedDateString() ?>
+                </span>
+            </td>
+
+            <td class="<?= $i ? '' : 'tw-border-t-2' ?> meta">
+                <div class="dropdown">
+                    <button class="btn btn-light btn-sm tw-my-0 tw-py-0" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        &middot;&middot;&middot;
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                        <?php if( $file->isViewable() ): ?>
+                            <a class="dropdown-item" href="<?= route( 'docstore-file@download', ['file' => $file->id] ) ?>">Download</a>
+                        <?php endif; ?>
+                        <?php if( Auth::check() && Auth::user()->isSuperUser() ): ?>
+                            <a class="dropdown-item list-info-btn" data-object-type="file" href="#" data-url="<?= route( "docstore-file@info", [ "file" => $file ] ) ?>">Metadata</a>
+                        <?php endif; ?>
+                        <a class="dropdown-item" href="#"
+                           onclick="bootbox.alert({ message: 'SHA checksums can be used to check the authenticity / integrity of files.<br><br><?= $file->sha256 ? "SHA256 checksum: [<code>" . $t->ee( $file->sha256 ) . "</code>]" : "there is no sha256 checksum registered for this file." ?>', size: 'large' }); return false;">Show SHA256</a>
+
+                        <?php if( Auth::check() && Auth::user()->isSuperUser() ): ?>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="<?= route( 'docstore-log@unique-list', [ 'file' => $file ] ) ?>">Unique Downloads</a>
+                            <a class="dropdown-item" href="<?= route( 'docstore-log@list', [ 'file' => $file ] ) ?>">All Downloads</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="<?= route( "docstore-file@edit", [ "file" => $file ] ) ?>">Edit</a>
+                            <a class="dropdown-item list-delete-btn" data-object-type="file" href="#" data-url="<?= route( "docstore-file@delete", [ "file" => $file ] ) ?>">Delete</a>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </td>
+        </tr>
+
+    <?php endforeach; ?>
+
+    <tr>
+        <td class="top" colspan="7"></td>
+    </tr>
+
+    </tbody>
+</table>
+</div>
+
+
+
 
         </div>
     </div>
