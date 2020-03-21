45629b2f5533c9e33da71442ea1201951a76a71e
Barry O'Donovan <barry@islandbridgenetworks.ie>
2020-03-21 09:18:13 +0000

    Version bump to v5.5.0

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 # Basic version information
 #
 
-define( 'APPLICATION_VERSION', '5.4.1' );
-define( 'APPLICATION_VERDATE', '2020030600' );
+define( 'APPLICATION_VERSION', '5.5.0' );
+define( 'APPLICATION_VERDATE', '2020032100' );
 
