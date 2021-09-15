ebd32b68866f62f4fdd5e410ffda9fab8b6156a1
Barry O'Donovan <barry@opensolutions.ie>
2021-09-15 20:43:22 +0100

    [IM] Update transit ASN filtering and allow greater control - closes inex/IXP-Manager#716
    Docs at: https://docs.ixpmanager.org/features/routers/#filtering-known-transit-networks

--- resources/views/api/v4/router/collector/bird2/filter-transit-networks.foil.php
+++ resources/views/api/v4/router/collector/bird2/filter-transit-networks.foil.php
@@ -10,33 +10,71 @@
 ########################################################################################
 ########################################################################################
 
+<?php
+    // default transit networks to block
+    $no_transit_asns = [
+        174   => 'Cogent',
+        701   => 'UUNET',
+        1299  => 'Telia',
+        2914  => 'NTT Communications',
+        3257  => 'GTT Backbone',
+        3320  => 'Deutsche Telekom AG (DTAG)',
+        3356  => 'Level3',
+        3491  => 'PCCW',
+        4134  => 'Chinanet',
+        5511  => 'Orange opentransit',
+        6453  => 'Tata Communications',
+        6461  => 'Zayo Bandwidth',
+        6762  => 'Seabone / Telecom Italia',
+        6830  => 'Liberty Global',
+        7018  => 'AT&T',
+    ];
 
-define TRANSIT_ASNS = [ 174,                  # Cogent
-                        209,                  # Qwest (HE carries this on IXPs IPv6 (Jul 12 2018))
-                        701,                  # UUNET
-                        702,                  # UUNET
-                        1239,                 # Sprint
-                        1299,                 # Telia
-                        2914,                 # NTT Communications
-                        3257,                 # GTT Backbone
-                        3320,                 # Deutsche Telekom AG (DTAG)
-                        3356,                 # Level3
-                        3549,                 # Level3
-                        3561,                 # Savvis / CenturyLink
-                        4134,                 # Chinanet
-                        5511,                 # Orange opentransit
-                        6453,                 # Tata Communications
-                        6762,                 # Seabone / Telecom Italia
-                        7018 ];               # AT&T
+    // possible overrides - exclusions from the above:
+    if( count( config( 'ixp.no_transit_asns.exclude' ) ) ) {
+        foreach( config( 'ixp.no_transit_asns.exclude' ) as $asn ) {
+            if( isset( $no_transit_asns[$asn] ) ) {
+                unset( $no_transit_asns[$asn] );
+            }
+        }
+    }
+
+    // possible overrides - complete replacement:
+    if( config( 'ixp.no_transit_asns.override' ) !== false ) {
+        $no_transit_asns = [];
+        foreach( config( 'ixp.no_transit_asns.override' ) as $asn ) {
+            $no_transit_asns[ $asn ] = 'Override from .env file';
+        }
+    }
+?>
+
+# Filtering the following ASNs:
+#
+<?php foreach( $no_transit_asns as $asn => $desc ): ?>
+# <?= $asn ?> - <?= $desc ?>
+
+<?php endforeach; ?>
+
+<?php if( count( $no_transit_asns ) === 0 ): ?>
+# .env file has disabled transit ASN filtering with an empty IXP_NO_TRANSIT_ASNS_OVERRIDE setting:
+function filter_has_transit_path()
+{
+    return false;
+}
+
+<?php else: ?>
+define TRANSIT_ASNS = [ <?= implode( ', ', array_keys( $no_transit_asns ) ) ?> ];
 
 function filter_has_transit_path()
 int set transit_asns;
 {
     transit_asns = TRANSIT_ASNS;
-    if (bgp_path.first !~ transit_asns && bgp_path ~ transit_asns) then {
+    if (bgp_path ~ transit_asns) then {
         bgp_large_community.add( IXP_LC_FILTERED_TRANSIT_FREE_ASN );
         return true;
     }
 
     return false;
 }
+
+<?php endif; ?>
