fb155a4a7869545481499b1bde40ddaa8296e3f3
Barry O'Donovan <barry@opensolutions.ie>
2021-11-14 09:46:14 +0000

    Update version identifier to v6.2.0

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 # Basic version information
 #
 
-define( 'APPLICATION_VERSION', '6.1.0' );
-define( 'APPLICATION_VERDATE', '2021091500' );
+define( 'APPLICATION_VERSION', '6.2.0' );
+define( 'APPLICATION_VERDATE', '2021111400' );
 
