ed8ee84063da69d97aaa16bbb9eb786e9ee8da2f
Barry O'Donovan <barry@opensolutions.ie>
2021-11-28 09:26:25 +0000

    Bring .env and config/mail.php back into sync and match defaults

--- config/mail.php
+++ config/mail.php
@@ -59,7 +59,7 @@ return [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'localhost'),
             'port' => env('MAIL_PORT', 25),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'encryption' => env('MAIL_ENCRYPTION', false),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
