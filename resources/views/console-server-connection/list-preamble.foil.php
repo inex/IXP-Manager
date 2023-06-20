fddbc38adb477c9cd46a462655ffed96d3d42229
Barry O'Donovan <barry@opensolutions.ie>
2023-06-20 08:49:45 +0100

    [BF] Fix a number of XSS issues
    Thanks to the GRNET IT Security Team for responsibly disclosing these issues.

--- resources/views/console-server-connection/list-preamble.foil.php
+++ resources/views/console-server-connection/list-preamble.foil.php
@@ -1,7 +1,7 @@
 <?php if( isset($t->data[ 'params'][ "cs" ]) ): ?>
     <div>
         <h2>
-            Ports for Console Server: <?= $t->data[ 'params'][ "css" ][$t->data[ 'params'][ "cs" ] ][ 'name' ] ?><br><br>
+            Ports for Console Server: <?= $t->ee( $t->data[ 'params'][ "css" ][$t->data[ 'params'][ "cs" ] ][ 'name' ] ) ?><br><br>
         </h2>
     </div>
 <?php endif;?>
--- resources/views/console-server-connection/page-header-preamble.foil.php
+++ resources/views/console-server-connection/page-header-preamble.foil.php
@@ -2,7 +2,7 @@
     <div class="btn-group btn-group-sm" role="group">
         <div class="btn-group btn-group-sm">
             <button type="button" class="btn btn-white dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <?= isset( $t->data[ 'params'][ "cs" ] ) ? $t->data[ 'params'][ "css" ][$t->data[ 'params'][ "cs" ] ][ 'name' ] : "All Console Server Ports" ?> <span class="caret"></span>
+                <?= isset( $t->data[ 'params'][ "cs" ] ) ? $t->ee( $t->data[ 'params'][ "css" ][$t->data[ 'params'][ "cs" ] ][ 'name' ] ) : "All Console Server Ports" ?> <span class="caret"></span>
             </button>
 
             <div class="dropdown-menu dropdown-menu-right scrollable-dropdown">
@@ -11,7 +11,7 @@
                 </a>
                 <div class="dropdown-divider"></div>
                 <?php foreach( $t->data[ 'params'][ "css" ] as $css ): ?>
-                    <a class="dropdown-item <?= isset( $t->data[ 'params'][ "cs" ] ) && $t->data[ 'params'][ "cs" ] === $css[ 'id' ] ? 'active' : '' ?>" href="<?= route( "console-server-connection@listPort", [ "cs" => $css[ 'id' ] ] ) ?>"><?= $css[ 'name' ] ?></a>
+                    <a class="dropdown-item <?= isset( $t->data[ 'params'][ "cs" ] ) && $t->data[ 'params'][ "cs" ] === $css[ 'id' ] ? 'active' : '' ?>" href="<?= route( "console-server-connection@listPort", [ "cs" => $css[ 'id' ] ] ) ?>"><?= $t->ee( $css[ 'name' ] ) ?></a>
                 <?php endforeach; ?>
             </div>
         </div>
