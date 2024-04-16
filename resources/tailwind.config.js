63e992810b2c0406951664a859db2eced14a75ce
Kiss László <griphons@gmail.com>
2024-04-16 17:27:53 +0100

    Tailwind Upgrade to V3

--- resources/tailwind.config.js
+++ resources/tailwind.config.js
@@ -41,7 +41,12 @@ module.exports = {
 
   prefix: 'tw-',
 
-
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.foil.php',
+    './resources/**/*.js',
+    './resources/**/*.jsx',
+  ],
 
   theme: {
 
@@ -974,73 +979,6 @@ module.exports = {
       */
 
   }, // theme
-
-  variants: {
-    appearance: ['responsive'],
-    backgroundAttachment: ['responsive'],
-    backgroundColor: ['responsive', 'hover', 'focus'],
-    backgroundPosition: ['responsive'],
-    backgroundRepeat: ['responsive'],
-    backgroundSize: ['responsive'],
-    borderCollapse: [],
-    borderColor: ['responsive', 'hover', 'focus'],
-    borderRadius: ['responsive', 'hover'],
-    borderStyle: ['responsive', 'hover'],
-    borderWidth: ['responsive', 'hover'],
-    cursor: ['responsive'],
-    display: ['responsive'],
-    flexDirection: ['responsive'],
-    flexWrap: ['responsive'],
-    alignItems: ['responsive'],
-    alignSelf: ['responsive'],
-    justifyContent: ['responsive'],
-    alignContent: ['responsive'],
-    flex: ['responsive'],
-    flexGrow: ['responsive'],
-    flexShrink: ['responsive'],
-    float: ['responsive'],
-    fontFamily: ['responsive'],
-    fontWeight: ['responsive', 'hover', 'focus'],
-    height: ['responsive'],
-    lineHeight: ['responsive'],
-    listStylePosition: ['responsive'],
-    listStyleType: ['responsive'],
-    margin: ['responsive'],
-    maxHeight: ['responsive'],
-    maxWidth: ['responsive'],
-    minHeight: ['responsive'],
-    minWidth: ['responsive'],
-    negativeMargin: ['responsive'],
-    objectFit: ['responsive'],
-    objectPosition: ['responsive'],
-    opacity: ['responsive'],
-    outline: ['focus'],
-    overflow: ['responsive'],
-    padding: ['responsive'],
-    pointerEvents: ['responsive'],
-    position: ['responsive'],
-    inset: ['responsive'],
-    resize: ['responsive'],
-    boxShadow: ['responsive', 'hover', 'focus'],
-    fill: [],
-    stroke: [],
-    tableLayout: ['responsive'],
-    textAlign: ['responsive'],
-    textColor: ['responsive', 'hover', 'focus'],
-    fontSize: ['responsive'],
-    fontStyle: ['responsive'],
-    textTransform: ['responsive'],
-    textDecoration: ['responsive', 'hover', 'focus'],
-    fontSmoothing: ['responsive'],
-    letterSpacing: ['responsive'],
-    userSelect: ['responsive'],
-    verticalAlign: ['responsive'],
-    visibility: ['responsive'],
-    whitespace: ['responsive'],
-    wordBreak: ['responsive'],
-    width: ['responsive'],
-    zIndex: ['responsive'],
-  },
 
 
   /*
