f58d5bfc25bfc3dfe0835e03afb7ea7b8ed30cd9
Barry O'Donovan <barry@opensolutions.ie>
2024-06-09 14:27:14 +0100

    Stamp v6.4.1

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 # Basic version information
 #
 
-define( 'APPLICATION_VERSION', '6.4.0' );
-define( 'APPLICATION_VERDATE', '2024052400' );
+define( 'APPLICATION_VERSION', '6.4.1' );
+define( 'APPLICATION_VERDATE', '2024060900' );
 
