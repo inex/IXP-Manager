7295b1a3e3444869c0a2fef1557973ffd3ff7c3f
Kiss László <griphons@gmail.com>
2024-07-30 17:54:47 +0100

    fix psalm issue by suppress

--- app/Models/Aggregators/BgpSessionDataAggregator.php
+++ app/Models/Aggregators/BgpSessionDataAggregator.php
@@ -32,9 +32,7 @@ use IXP\Exceptions\GeneralException as IXP_Exception;
  * @method static Builder|BgpSessionDataAggregator whereTimestamp($value)
  * @method static Builder|BgpSessionDataAggregator whereVlan($value)
  * @mixin \Eloquent
- * @psalm-mutation-free
- * @psalm-suppress InvalidTemplateParam
- **/
+ */
 class BgpSessionDataAggregator extends BgpSessionData
 {
 
