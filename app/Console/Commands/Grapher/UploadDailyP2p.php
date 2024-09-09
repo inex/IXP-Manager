9cf52ac222ab6a77f292b4fff8e8a39d28fae5f2
Barry O'Donovan <barry@opensolutions.ie>
2024-09-09 09:38:00 +0100

    Quick refactor

--- app/Console/Commands/Grapher/UploadDailyP2p.php
+++ app/Console/Commands/Grapher/UploadDailyP2p.php
@@ -4,6 +4,7 @@ namespace IXP\Console\Commands\Grapher;
 
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Database\Query\Builder;
 use IXP\Models\Aggregators\VlanInterfaceAggregator;
 use IXP\Models\Customer;
 use Log;
@@ -16,41 +17,41 @@ class UploadDailyP2p extends Command
      * @var string
      */
     protected $signature = 'grapher:upload-daily-p2p 
-                    {date : target day in YYY-MM-DD format}
-                    {--C|customer-id= : Customer ID}';
+                    {day : target day in YYY-MM-DD format}
+                    {--customer-id= : Customer ID}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Store daily p2p traffic of customer(s)';
+    protected $description = 'Calculate and save daily p2p traffic stats between members';
 
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle(): int
     {
-        $customerId = $this->option('customer-id');
-
-        $date = $this->argument('date');
-        $startDate = Carbon::parse($date . " 00:00:00");
-        $endDate = Carbon::parse($date . " 23:59:59");
-
-        if($customerId) {
-            $allCustomer = Customer::currentActive(true,true,true)
-                ->where('id', $customerId)
-                ->get();
-        } else {
-            $allCustomer = Customer::currentActive(true,true,true)->get();
+        if( !preg_match( "/\d\d\d\d\-\d\d\-\d\d", $this->argument('day') ) ) {
+            $this->error("Invalid day parameter - expected format is " . now()->subDay()->format('Y-m-d') );
+            return -1;
         }
 
-        if($allCustomer) {
-            foreach($allCustomer as $c) {
+        $start     = Carbon::parse( $this->argument('day') . ' 00:00:00' );
+        $end       = $start->copy()->endOfDay();
+        $startTime = microtime(true);
+
+        Customer::currentActive(true,true,true)
+            ->when( $this->option('customer-id'), function (Builder $query, string $cid) {
+                $query->where('id', $cid);
+            })
+            ->each( function( Customer $c ) use ( $start, $end, $startTime ) {
+
+                $itertime = microtime(true);
+
                 if($this->isVerbosityNormal()) {
-                    Log::debug("Processing $c->name for date $date");
+                    Log::debug("Processing {$c->name} for " . $start->format('Y-m-d'));
                 }
-                $startTime = microtime();
 
                 $stats = [];
                 $peerId = null;
@@ -118,14 +119,11 @@ class UploadDailyP2p extends Command
 */
 
 
-                $endTime = microtime();
-                $workTime = $endTime - $startTime;
                 if($this->isVerbosityNormal()) {
-                    Log::debug("Completed $c->name took $workTime seconds");
+                    Log::debug("Completed {$c->name} in " . (microtime(true) - $itertime) . " seconds");
                 }
-            }
+            });
 
-        }
-
+        return 0;
     }
 }
