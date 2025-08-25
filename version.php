b6dc36673cf8bdda64107a58b3b5c3e776e8be19
Barry O'Donovan <barry@opensolutions.ie>
2025-08-25 10:40:40 +0100

    Tag v7.0.0 🚀

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2009 - 2021 Internet Neutral Exchange Association Company Limited By Guarantee.
+ * Copyright (C) 2009 - 2025 Internet Neutral Exchange Association Company Limited By Guarantee.
  * All Rights Reserved.
  *
  * This file is part of IXP Manager.
@@ -25,7 +25,6 @@
 # Basic version information
 #
 
-define( 'APPLICATION_VERSION', '6.4.1' );
-define( 'APPLICATION_VERDATE', '2024060900' );
-define( 'DOCUMENTATION_VERSION', '6.4' );
-
+define( 'APPLICATION_VERSION', '7.0.0' );
+define( 'APPLICATION_VERDATE', '2025082500' );
+define( 'DOCUMENTATION_VERSION', '7.0' );
