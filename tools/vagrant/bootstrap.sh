fcbca2d11c2eec5836a23701bbbfe8c843966850
Barry O'Donovan <barry@opensolutions.ie>
2025-08-28 20:41:14 +0100

    [DEV] Birdseye branch php83 no longer exists

--- tools/vagrant/bootstrap.sh
+++ tools/vagrant/bootstrap.sh
@@ -357,7 +357,6 @@ echo "Setting up Birdseye / looking glasses..."
 git clone https://github.com/inex/birdseye.git /srv/birdseye &>/dev/null
 cd /srv/birdseye
 git config --global --add safe.directory /srv/birdseye
-git checkout php83 &>/dev/null
 COMPOSER_ALLOW_SUPERUSER=1 composer install &>/dev/null
 
 cat >/etc/apache2/sites-enabled/birdseye.conf <<END_APACHE
