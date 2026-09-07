4390bf0cb9ab52dc791b9b765f809b9b15090246
Thomas Kerin <thomas@islandbridgenetworks.ie>
2026-09-07 12:26:05 +0100

    cb-functions: move 'selectNextSwitchPort' and 'setSwitchPort' to js/create-wizard.foil.php - they're only used on the create-wizard page and don't need to be exported to other pages

--- resources/views/interfaces/core-bundle/js/create-wizard.foil.php
+++ resources/views/interfaces/core-bundle/js/create-wizard.foil.php
@@ -309,6 +309,73 @@
         }
     }
 
+    /**
+     * set data to the switch port dropdown when we select a switcher
+     */
+    function setSwitchPort( sside, core_link_form, action, edit )
+    {
+        let datas;
+        let switchId = $( "#switch-" + sside ).val();
+
+        if( $( ".core-link-form" ).length > 0 || edit ) {
+            let dd_switch_port = core_link_form.find( `.sp-${ sside }` );
+            dd_switch_port.html( `<option value="">Loading please wait</option>\n` ).trigger( 'change.select2' );
+
+            if( switchId != null && switchId !== '' ) {
+                let url = "<?= url( '/admin/api/v4/switch' )?>/" + switchId + "/ports";
+
+                // query for switch ports used on this side so they can be excluded from results
+                let selectedSps = $(`[id=sp-${sside}] :selected`).map(function () {
+                    return this.value;
+                }).get().filter(function (v) { return v !== ''; });
+
+                datas = {
+                    types : [ <?= \IXP\Models\SwitchPort::TYPE_UNSET ?>, <?= \IXP\Models\SwitchPort::TYPE_CORE ?> ],
+                    notAssignToPI: true,
+                    piNull: true,
+                    spIdsExcluded : !edit ? selectedSps : []
+                };
+
+                $.ajax( url , {
+                    data: datas,
+                    method: "GET",
+                    _token : "<?= csrf_token() ?>"
+                })
+                    .done( function( data ) {
+                        let options = `<option value="">Choose a switch port</option>\n`;
+
+                        $.each( data.ports, function( key, value ){
+                            let swPortName = htmlEntities(value.name);
+                            options += `<option value="${value.id}">${swPortName} (${value.type})</option>\n`;
+                        });
+
+                        dd_switch_port.html( options );
+
+                        if( action === 'addBtn' ) {
+                            selectNextSwitchPort( dd_switch_port, sside );
+                        }
+                    })
+                    .fail( function() {
+                        alert( `Error running ajax query for ${url}` );
+                        throw new Error( `Error running ajax query for ${url}` );
+                    })
+                    .always( function() {
+                        dd_switch_port.trigger('change.select2');
+                    });
+            }
+        }
+    }
+
+    /**
+     * Select the switch port depending of the previous core links
+     */
+    function selectNextSwitchPort( dd_switch_port , side ) {
+        let previous_dd_sp = dd_switch_port.closest( '.core-link-form' ).prev().find( `.sp-${side}` );
+        let sp_val = previous_dd_sp.find( ":selected" ).next().val();
+
+        dd_switch_port.val( sp_val );
+        dd_switch_port.closest( '.core-link-form' ).find( `.hidden-sp-${side}` ).val( sp_val );
+    }
 
     /**
      * Copy the switch dropdown from the side A to B excluding the switch selected in side A
